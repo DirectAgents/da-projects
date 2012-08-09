d93cbd1a73574d0b7995f52fe6043533b90484a9
Aaron Anodide <greentunes@gmail.com>
2012-08-08 23:12:39 -0700

    testing a winforms hosted wpf user control

---
+++ 1/EomTool/EomApp1/Screens/Wpf/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EomApp1.Screens.Wpf
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Utilities.PlaceWpfControlIntoForm<EomApp.Controls.Wpf.Cone>(this);
+        }
+    }
+}
---
+++ 1/EomTool/EomApp1/Screens/Wpf/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace EomApp1.Screens.Wpf
+{
+    public static class Utilities
+    {
+        public static void PlaceWpfControlIntoForm<T>(Form form) where T : System.Windows.Controls.UserControl, new()
+        {
+            form.Controls.Add(new ElementHost
+            {
+                Dock = DockStyle.Fill,
+                Child = new T()
+            });
+        }
+    }
+}
