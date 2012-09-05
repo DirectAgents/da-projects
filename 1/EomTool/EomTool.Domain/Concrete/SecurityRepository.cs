e85422925dc54ef232dd92921b8eb27a4fad25a5
kslesinsky <kevin@directagents.com>
2012-09-05 16:48:51 -0400

    get all security groups

--- 1/EomTool/EomTool.Domain/Concrete/SecurityRepository.cs
+++ 1/EomTool/EomTool.Domain/Concrete/SecurityRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using EomTool.Domain.Abstract;
 using EomTool.Domain.Entities;
+using System.Security.Principal;
 
 namespace EomTool.Domain.Concrete
 {
@@ -18,8 +19,15 @@ namespace EomTool.Domain.Concrete
         public Group WindowsIdentityGroup(string windowsIdentity)
         {
             var groups = db.Groups.ToList();
-            var identityGroups = groups.Where(c => c.WindowsIdentity.Split(',').Any(d => d.ToUpper() == windowsIdentity.ToUpper()));
+            var identityGroups = groups.Where(g => g.WindowsIdentity.Split(',').Any(wi => wi.ToUpper() == windowsIdentity.ToUpper()));
             return identityGroups.FirstOrDefault();
+        }
+
+        public IEnumerable<Group> GroupsForUser(IPrincipal user)
+        {
+            var allGroups = db.Groups.ToList();
+            var groups = allGroups.Where(g => g.WindowsIdentity.Split(',').Any(wi => user.IsInRole(wi)));
+            return groups;
         }
     }
 }
