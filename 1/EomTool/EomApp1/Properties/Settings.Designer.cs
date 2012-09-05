c5af2b8f822cb3bb192773e669c9d1c85899d2ea
kslesinsky <kevin@directagents.com>
2012-09-05 14:26:48 -0400

    disable MBA if can't verify

--- 1/EomTool/EomApp1/Properties/Settings.Designer.cs
+++ 1/EomTool/EomApp1/Properties/Settings.Designer.cs
@@ -244,7 +244,7 @@ namespace EomApp1.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool TestMode {
             get {
                 return ((bool)(this["TestMode"]));
