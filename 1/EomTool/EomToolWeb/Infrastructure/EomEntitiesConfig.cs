2e4d4c042e82ba5460971efa71f10cace04a7036
Aaron Anodide <greentunes@gmail.com>
2012-09-06 10:38:28 -0700

    config tweak

--- 1/EomTool/EomToolWeb/Infrastructure/EomEntitiesConfig.cs
+++ 1/EomTool/EomToolWeb/Infrastructure/EomEntitiesConfig.cs
@@ -25,16 +25,24 @@ namespace EomToolWeb.Infrastructure
         {
             var config = WebConfigurationManager.OpenWebConfiguration("/EomToolWeb");
             var eomToolConfig = config.GetSection<EomToolWebConfigSection>();
-            
-            string query = "select top 1 connection_string from DADatabase where effective_date = @eomDate";
-            string connectionString = SqlUtility.ExecuteScalar<string>(eomToolConfig.MasterConnectionString, query, eomDate);
-            
+            string connectionString;
+
+            if (eomToolConfig.DebugMode)
+            {
+                connectionString = @"data source=biz2\da;initial catalog=zDADatabaseJuly2012Test2;Integrated Security=True";
+            }
+            else
+            {
+                string query = "select top 1 connection_string from DADatabase where effective_date = @eomDate";
+                connectionString = SqlUtility.ExecuteScalar<string>(eomToolConfig.MasterConnectionString, query, eomDate);
+            }
+
             if (connectionString == null)
                 throw new Exception("Cannot determine EOM database connection string for date " + eomDate.ToString());
-            
+
             if (eomToolConfig.ReplaceIntegratedSecurityWithSALogin)
                 connectionString = connectionString.Replace("Integrated Security=True", string.Format("User=sa;Password={0}", eomToolConfig.SAPassword));
-            
+
             return connectionString;
         }
     }
