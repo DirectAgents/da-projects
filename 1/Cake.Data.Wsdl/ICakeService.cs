db7a30080d51a0c3b7b5159c36a915cbe6a55104
Aaron Anodide <greentunes@gmail.com>
2012-09-11 02:36:34 -0700

    Cake web service client

---
+++ 1/Cake.Data.Wsdl/ICakeService.cs
@@ -0,0 +1,16 @@
+﻿using Cake.Data.Wsdl.ExportService;
+using Cake.Data.Wsdl.ReportsService;
+using System.Collections.Generic;
+
+namespace Cake.Data.Wsdl
+{
+    //[LogMethodBoundary]
+    public interface ICakeService
+    {
+        conversion[] Conversions();
+        Cake.Data.Wsdl.ExportService.advertiser[] ExportAdvertisers();
+        Cake.Data.Wsdl.ExportService.affiliate[] ExportAffiliates();
+        IEnumerable<campaign[]> ExportCampaigns();
+        offer1[] ExportOffers();
+    }
+}
---
+++ 1/Cake.Data.Wsdl/offer1.partial.cs
@@ -0,0 +1,21 @@
+﻿namespace Cake.Data.Wsdl.ExportService
+{
+    public partial class offer1
+    {
+        public string StatusName
+        {
+            get
+            {
+                return this.offer_status.offer_status_name;
+            }
+        }
+
+        public string AdvertiserId
+        {
+            get
+            {
+                return this.advertiser.advertiser_id.ToString();
+            }
+        }
+    }
+}
