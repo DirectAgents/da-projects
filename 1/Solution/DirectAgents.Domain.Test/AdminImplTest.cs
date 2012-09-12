fc36f66e8c566ce30a7dfad57a3e1be58d5a0696
Aaron Anodide <greentunes@gmail.com>
2012-09-12 00:19:56 -0700

    added TinyMCE script

---
+++ 1/Solution/DirectAgents.Domain.Test/AdminImplTest.cs
@@ -0,0 +1,87 @@
+﻿using DirectAgents.Domain.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DirectAgents.Domain.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for AdminImplTest and is intended
+    ///to contain all AdminImplTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class AdminImplTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for ReCreateDatabase
+        ///</summary>
+        [TestMethod()]
+        public void ReCreateDatabaseTest()
+        {
+            AdminImpl target = new AdminImpl();
+            target.ReCreateDatabase();
+        }
+
+        /// <summary>
+        ///A test for LoadCampaigns
+        ///</summary>
+        [TestMethod()]
+        public void LoadCampaignsTest()
+        {
+            AdminImpl target = new AdminImpl();
+            target.LoadCampaigns();
+        }
+    }
+}
---
+++ 1/Solution/DirectAgents.Domain.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DirectAgents.Domain.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("DirectAgents.Domain.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b955c2b8-dc10-4c22-b3ec-d18a7ea44e1f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
