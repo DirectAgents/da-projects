9cf879dc9dc1599e8129696743ec5796267e4b7a
kslesinsky <kevin@directagents.com>
2012-11-15 11:52:32 -0500

    create Questions class (repo-like)

--- 2/LendingTree/LTWeb/Controllers/QuestionsController.cs
+++ 2/LendingTree/LTWeb/Controllers/QuestionsController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
+﻿using System.Reflection;
 using System.Web.Mvc;
-using System.Xml;
-using System.Xml.Linq;
 using LTWeb.Models;
 using LTWeb.Service;
 
@@ -20,7 +14,7 @@ namespace LTWeb.Controllers
 
         public ActionResult Show(int q = 0, bool test = false)
         {
-            var questions = GetQuestionVMs();
+            var questions = Questions.GetQuestionVMs();
             var model = questions[q];
  
             if (test)
@@ -49,7 +43,7 @@ namespace LTWeb.Controllers
                 return null;
             else
             {
-                var nextQuestion = GetNextQuestionVM(questionKey, sessionModel);
+                var nextQuestion = Questions.GetNextQuestionVM(questionKey, sessionModel);
                 if (nextQuestion == null)
                     return Content("no more questions");
                 else
@@ -62,87 +56,5 @@ namespace LTWeb.Controllers
             return Content("thank you");
         }
 
-        // returns null if there is no next question
-        private QuestionVM GetNextQuestionVM(string completedKey, ILendingTreeModel ltModel)
-        {
-            var questions = GetQuestionVMs();
-            bool foundCompletedQuestion = false;
-            QuestionVM nextQuestion = null;
-            int i = 0;
-            while ((!foundCompletedQuestion || nextQuestion == null) && i < questions.Length)
-            {
-                var question = questions[i];
-                if (question.Key == completedKey)
-                {
-                    foundCompletedQuestion = true;
-                }
-                else if (foundCompletedQuestion)
-                {
-                    if (string.IsNullOrEmpty(question.DependencyKey))
-                    {
-                        nextQuestion = question;
-                    }
-                    else
-                    {
-                        object qValue = ltModel.GetType().GetProperty(question.DependencyKey).GetValue(ltModel);
-                        if (qValue is string)
-                        {
-                            if ((string)qValue == question.DependencyValue)
-                                nextQuestion = question;
-                        }
-                        else if (qValue is bool)
-                        {
-                            if ((bool)qValue == bool.Parse(question.DependencyValue))
-                                nextQuestion = question;
-                        }
-                        else
-                        {
-                            throw new Exception("invalid value");       
-                        }
-                    }
-                }
-                i++;
-            }
-            return nextQuestion;
-        }
-
-        private QuestionVM[] GetQuestionVMs()
-        {
-            List<QuestionVM> questionsList = new List<QuestionVM>();
-            var xelement = XElement.Load(Request.MapPath("~/App_Data/questions.xml"));
-            int i = 0;
-            foreach (var questionEl in xelement.Elements("question"))
-            {
-                var question = new QuestionVM()
-                {
-                    QuestionIndex = i++,
-                    Key = questionEl.Attribute("key").Value,
-                    Text = questionEl.Attribute("text").Value,
-                    AnswerType = questionEl.Attribute("answertype").Value
-                };
-
-                var xattr = questionEl.Attribute("subtext");
-                if (xattr != null) question.Subtext = xattr.Value;
-
-                xattr = questionEl.Attribute("dependencykey");
-                if (xattr != null) question.DependencyKey = xattr.Value;
-
-                xattr = questionEl.Attribute("dependencyvalue");
-                if (xattr != null) question.DependencyValue = xattr.Value;
-
-                question.Options = new List<OptionVM>();
-                foreach (var optionEl in questionEl.Descendants("option"))
-                {
-                    OptionVM option = new OptionVM()
-                    {
-                        Text = optionEl.Value,
-                        Value = optionEl.Attribute("value").Value
-                    };
-                    question.Options.Add(option);
-                }
-                questionsList.Add(question);
-            }
-            return questionsList.ToArray();
-        }
     }
 }
---
+++ 2/LendingTree/LTWeb/Questions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Xml.Linq;
+using LTWeb.Models;
+using LTWeb.Service;
+
+namespace LTWeb
+{
+    public static class Questions
+    {
+        public static QuestionVM[] GetQuestionVMs()
+        {
+            List<QuestionVM> questionsList = new List<QuestionVM>();
+            var xelement = XElement.Load(HttpContext.Current.Request.MapPath("~/App_Data/questions.xml"));
+            int i = 0;
+            foreach (var questionEl in xelement.Elements("question"))
+            {
+                var question = new QuestionVM()
+                {
+                    QuestionIndex = i++,
+                    Key = questionEl.Attribute("key").Value,
+                    Text = questionEl.Attribute("text").Value,
+                    AnswerType = questionEl.Attribute("answertype").Value
+                };
+
+                var xattr = questionEl.Attribute("subtext");
+                if (xattr != null) question.Subtext = xattr.Value;
+
+                xattr = questionEl.Attribute("dependencykey");
+                if (xattr != null) question.DependencyKey = xattr.Value;
+
+                xattr = questionEl.Attribute("dependencyvalue");
+                if (xattr != null) question.DependencyValue = xattr.Value;
+
+                question.Options = new List<OptionVM>();
+                foreach (var optionEl in questionEl.Descendants("option"))
+                {
+                    OptionVM option = new OptionVM()
+                    {
+                        Text = optionEl.Value,
+                        Value = optionEl.Attribute("value").Value
+                    };
+                    question.Options.Add(option);
+                }
+                questionsList.Add(question);
+            }
+            return questionsList.ToArray();
+        }
+
+        // returns null if there is no next question
+        public static QuestionVM GetNextQuestionVM(string completedKey, ILendingTreeModel ltModel)
+        {
+            var questions = Questions.GetQuestionVMs();
+            bool foundCompletedQuestion = false;
+            QuestionVM nextQuestion = null;
+            int i = 0;
+            while ((!foundCompletedQuestion || nextQuestion == null) && i < questions.Length)
+            {
+                var question = questions[i];
+                if (question.Key == completedKey)
+                {
+                    foundCompletedQuestion = true;
+                }
+                else if (foundCompletedQuestion)
+                {
+                    if (string.IsNullOrEmpty(question.DependencyKey))
+                    {
+                        nextQuestion = question;
+                    }
+                    else
+                    {
+                        object qValue = ltModel.GetType().GetProperty(question.DependencyKey).GetValue(ltModel);
+                        if (qValue is string)
+                        {
+                            if ((string)qValue == question.DependencyValue)
+                                nextQuestion = question;
+                        }
+                        else if (qValue is bool)
+                        {
+                            if ((bool)qValue == bool.Parse(question.DependencyValue))
+                                nextQuestion = question;
+                        }
+                        else
+                        {
+                            throw new Exception("invalid value");
+                        }
+                    }
+                }
+                i++;
+            }
+            return nextQuestion;
+        }
+
+    }
+}
