d1ee3d27f49e2d0042df1a5523bae18723a097d4
kslesinsky <kevin@directagents.com>
2012-11-15 15:47:15 -0500

    lt-script.js

---
+++ 2/LendingTree/LTWeb/Scripts/lt-script.js
@@ -0,0 +1,11 @@
+﻿function Save() {
+    var form = $('#questionForm');
+    $.ajax({
+        type: "POST",
+        url: form.attr('action'),
+        data: form.serialize(),
+        success: function (response) {
+            alert('success!');
+        }
+    });
+}
