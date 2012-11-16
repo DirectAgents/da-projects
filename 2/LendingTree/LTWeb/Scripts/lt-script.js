b18f25089e5a294ad30daf1b699603c56bf42fee
kslesinsky <kevin@directagents.com>
2012-11-16 12:01:39 -0500

    styling/placement- progbar/buttons/text

--- 2/LendingTree/LTWeb/Scripts/lt-script.js
+++ 2/LendingTree/LTWeb/Scripts/lt-script.js
@@ -1,4 +1,17 @@
-﻿function Save() {
+﻿function STMRCWindow(page, sHeight, swidth) {
+    if (swidth && sHeight) {
+        window.open(page, "CtrlWindow", ",toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=no,dependent=no,directories=no,width=" + swidth + ",height=" + sHeight + ",x=50,y=50");
+    }
+    else {
+        window.open(page, "CtrlWindow", "toolbar=no,menubar=no,location=no,scrollbars=yes,resizable=no,dependent=no,directories=no,width=500,height=540,x=50,y=50");
+    }
+}
+
+function InitializeProgressbar(barvalue) {
+    $('#progressbar').progressbar({ value: barvalue });
+}
+
+function Save() {
     var form = $('#questionForm');
     $('#form-fields').load(form.attr('action'), form.serialize());
 }
