c58574699cb7d93e3b60b3fe8f1a62f00c45feb7
Aaron Anodide <greentunes@gmail.com>
2012-11-24 12:16:02 -0800

    helper to deserialize xml

---
+++ 2/LendingTree/LTWeb.Common/XmlUtility.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace LTWeb.Common
+{
+    public static class XmlUtility
+    {
+        public static T Deserialize<T>(string xml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (TextReader reader = new StringReader(xml))
+            {
+                return (T)xmlSerializer.Deserialize(reader);
+            }
+        }
+    }
+}
