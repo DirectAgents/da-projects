8f36d7f9e2e02c3dc4abcb6058f5c62cea8c3acf
unknown <kslesinsky@.DirectAgents.local>
2013-01-10 10:59:39 -0500

    HtmlHelper.FormatCurrency

---
+++ 1/EomTool/EomToolWeb/HtmlHelpers/FormatHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EomToolWeb.HtmlHelpers
+{
+    public static class FormatHelpers
+    {
+        static Dictionary<string, string> CurrMap = new Dictionary<string, string>
+        {
+            {"USD", "en-us"},
+            {"GBP", "en-gb"},
+            {"EUR", "de-de"},
+            {"AUD", "en-AU"}
+        };
+
+        public static HtmlString FormatCurrency(this HtmlHelper html, string currency, decimal amount)
+        {
+            if (currency == null || !CurrMap.ContainsKey(currency))
+                return new HtmlString(string.Format("{0:N2}", amount));
+            else
+                return new HtmlString(string.Format(CultureInfo.CreateSpecificCulture(CurrMap[currency]), "{0:C}", amount));
+        }
+    }
+}
--- 1/EomTool/EomToolWeb/Models/PaymentBatchesViewModel.cs
+++ 1/EomTool/EomToolWeb/Models/PaymentBatchesViewModel.cs
@@ -8,21 +8,5 @@ namespace EomToolWeb.Models
     {
         public IEnumerable<PaymentBatch> Batches { get; set; }
         public bool AllowHold { get; set; }
-
-        Dictionary<string, string> CurrMap = new Dictionary<string, string>
-        {
-            {"USD", "en-us"},
-            {"GBP", "en-gb"},
-            {"EUR", "de-de"},
-            {"AUD", "en-AU"}
-        };
-
-        public string FormatCurrency(string currency, decimal amount)
-        {
-            if (currency == null || !CurrMap.ContainsKey(currency))
-                return string.Format("{0:N2}", amount);
-            else
-                return string.Format(CultureInfo.CreateSpecificCulture(CurrMap[currency]), "{0:C}", amount);
-        }
     }
 }
