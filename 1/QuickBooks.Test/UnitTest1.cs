6379c54f71f37f19238980aebd9426a8d05987a7
Aaron Anodide <greentunes@gmail.com>
2013-02-06 12:37:17 -0800

    qb synch

---
+++ 1/QuickBooks.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickBooks.Services;
+using System.Linq;
+using System.Collections.Generic;
+using Accounting.Domain.Entities;
+
+namespace QuickBooks.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestQBServiceLoadAndExtractCustomers()
+        {
+            QuickBooksSynchService service;
+            service = new QuickBooksSynchService("Aaron Corp");
+            service.Load(service.Extract<Customer>(), true);
+        }
+    }
+
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestRepo()
+        {
+            using (var context = new AccountingContext())
+            {
+                var a = context.CompanyFiles;
+                foreach (var item in a)
+                {
+                    Console.WriteLine(item.CompanyId + ", " + item.CompanyName);
+                }
+            }
+        }
+    }
+}
--- 1/QuickBooks/UI/Presenters/QuickBooksSynchPresenter.cs
+++ 1/QuickBooks/UI/Presenters/QuickBooksSynchPresenter.cs
@@ -1,4 +1,5 @@
-﻿using QuickBooks.Services;
+﻿using Accounting.Domain.Entities;
+using QuickBooks.Services;
 using QuickBooks.UI.Models;
 using QuickBooks.UI.Views;
 using System.Collections.Generic;
@@ -12,7 +13,7 @@ namespace QuickBooks.UI.Presenters
         public QuickBooksSynchPresenter(IQuickBooksSynchView view)
             : base(view)
         {
-            this.quickBooksSynchService = new QuickBooksSynchService();
+            this.quickBooksSynchService = new QuickBooksSynchService("Aaron Corp");
             View.GetCompanyFiles += View_GetCompanyFiles;
             View.GetQuickBooksSynchSettings += View_GetQuickBooksSynchSettings;
             View.Go += View_Go;
@@ -20,20 +21,21 @@ namespace QuickBooks.UI.Presenters
 
         void View_Go(object sender, System.EventArgs e)
         {
-            var synchTargets = View.SynchTargets;
-            foreach (var target in synchTargets)
+            foreach (var target in View.SynchTargets)
             {
                 switch (target)
                 {
                     case QuickBooksSynchTargets.Customer:
-                        this.quickBooksSynchService.ExtractCustomers();
-                        this.quickBooksSynchService.LoadCustomers();
+                        this.quickBooksSynchService.Load(this.quickBooksSynchService.Extract<Customer>(), true);
                         break;
                     case QuickBooksSynchTargets.Invoice:
+                        this.quickBooksSynchService.Load(this.quickBooksSynchService.Extract<Customer>(), true);
                         break;
                     case QuickBooksSynchTargets.Payment:
+                        this.quickBooksSynchService.Load(this.quickBooksSynchService.Extract<Customer>(), true);
                         break;
                     case QuickBooksSynchTargets.CreditMemo:
+                        this.quickBooksSynchService.Load(this.quickBooksSynchService.Extract<Customer>(), true);
                         break;
                     default:
                         break;
@@ -52,7 +54,7 @@ namespace QuickBooks.UI.Presenters
 
         void View_GetCompanyFiles(object sender, GetCompanyFilesEventArgs e)
         {
-            var companyFiles = new List<CompanyFile>();
+            var companyFiles = new List<Models.CompanyFile>();
             companyFiles.Add(new Models.CompanyFile { Id = 1, Name = "US Company File" });
             companyFiles.Add(new Models.CompanyFile { Id = 2, Name = "INTL Company File" });
             e.CompanyFiles = companyFiles;
