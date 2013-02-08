8a3c5739cf56c0f57f9d0e055d178ae58f330c39
Kevin Slesinsky <kevin@directagents.com>
2013-02-08 14:08:07 -0500

    pub selector tool

---
+++ 1/EomTool/EomToolWeb/Models/SelectorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EomTool.Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EomToolWeb.Models
+{
+    public class SelectorViewModel
+    {
+        public string[] AccountingPeriods { get; set; }
+        public string AccountingPeriod { get; set; }
+        public int? AccountingStatus { get; set; }
+
+        public IQueryable<PublisherPayout> PayoutsQueryable { get; set; }
+        public IEnumerable<IGrouping<string, PublisherPayout>> PubGroups { get; set; }
+        public IEnumerable<PublisherPayout> PubPayouts { get; set; }
+    }
+}
