507b6e56ee401264470b94cbdf63bd29bd5b21b2
Aaron Anodide <greentunes@gmail.com>
2013-03-04 15:52:33 -0500

    beginnings of a web service (currently doesn't do anything)

---
+++ 2/Huggies/Huggies.Web/ws/IStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Huggies.Web.ws
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IStats" in both code and config file together.
+    [ServiceContract]
+    public interface IStats
+    {
+        [OperationContract]
+        StatsResult Get(DateTimeOffset lastChecked);
+    }
+}
---
+++ 2/Huggies/Huggies.Web/ws/Stats.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Huggies.Web.ws
+{
+    public class Stats : IStats
+    {
+        public StatsResult Get(DateTimeOffset lastChecked)
+        {
+            return new StatsResult
+                {
+                    NumNew = 2
+                };
+        }
+    }
+
+    public class StatsResult
+    {
+        public int NumNew { get; set; }
+    }
+}
