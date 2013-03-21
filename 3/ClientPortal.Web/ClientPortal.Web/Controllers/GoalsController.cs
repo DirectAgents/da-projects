8e7c42146e6535cf327a850a35346f97877da9b5
Aaron Anodide <greentunes@gmail.com>
2013-03-21 02:37:02 -0700

    started goals setup, public to gogrid

---
+++ 3/ClientPortal.Web/ClientPortal.Web/Controllers/GoalsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using ClientPortal.Web.Models;
+
+namespace ClientPortal.Web.Controllers
+{
+    public class GoalsController : Controller
+    {
+        //
+        // GET: /Goals/
+
+        public ActionResult Index()
+        {
+            return PartialView(new List<GoalVM>
+                {
+                    new GoalVM
+                        {
+                            Name = "goal name",
+                            Type = "Absolute|Percent",
+                            Offer = "offer name",
+                            Metric = "Conversions|Revenue",
+                            Target = 50
+                        }
+                });
+        }
+
+        public ActionResult Add()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Add(GoalVM goalVM)
+        {
+            return PartialView("Item", goalVM);
+        }
+    }
+}
---
+++ 3/ClientPortal.Web/ClientPortal.Web/Models/GoalVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientPortal.Web.Models
+{
+    public class GoalVM
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public string Offer { get; set; }
+
+        [Required]
+        public string Metric { get; set; }
+
+        [Required]
+        public decimal Target { get; set; }
+    }
+}
