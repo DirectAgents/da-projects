b6f0551ce8d6602eb3b9d33323de9cfe8bf08b4c
Kevin Slesinsky <kevin@directagents.com>
2013-03-22 12:26:42 -0400

    GoalTypeEnum & MetricEnum

--- 3/ClientPortal.Web/ClientPortal.Web/Controllers/GoalsController.cs
+++ 3/ClientPortal.Web/ClientPortal.Web/Controllers/GoalsController.cs
@@ -8,22 +8,29 @@ namespace ClientPortal.Web.Controllers
     {
         public ActionResult Index()
         {
+            var goalTypes = GetGoalTypes();
+            var metrics = GetMetrics();
+
             var goals = new List<GoalVM>
             {
                 new GoalVM
                 {
                     Name = "goal name 1",
-                    Type = GoalTypeEnum.Percent,
+                    Type = goalTypes[1],
+                    TypeId = goalTypes[1].Id,
                     Offer = "offer name 1",
-                    Metric = MetricTypeEnum.Conversions,
+                    Metric = metrics[0],
+                    MetricId = metrics[0].Id,
                     Target = 50
                 },
                 new GoalVM
                 {
                     Name = "goal name 2",
-                    Type = GoalTypeEnum.Absolute,
+                    Type = goalTypes[0],
+                    TypeId = goalTypes[0].Id,
                     Offer = "offer name 2",
-                    Metric = MetricTypeEnum.Revenue,
+                    Metric = metrics[1],
+                    MetricId = metrics[1].Id,
                     Target = 1000
                 },
             };
@@ -40,8 +47,8 @@ namespace ClientPortal.Web.Controllers
         {
             var defaultGoal = new GoalVM
             {
-                Type = GoalTypeEnum.Absolute,
-                Metric = MetricTypeEnum.Conversions
+                TypeId = GoalTypeEnum.Percent,
+                MetricId = MetricEnum.Revenue
             };
 
             return PartialView(defaultGoal);
@@ -53,25 +60,24 @@ namespace ClientPortal.Web.Controllers
             return PartialView("Item", goalVM);
         }
 
-/*
+
         private List<GoalTypeVM> GetGoalTypes()
         {
             var goalTypes = new List<GoalTypeVM> {
-                new GoalTypeVM() { Id = 1, Name = "Absolute" },
-                new GoalTypeVM() { Id = 2, Name = "Percent" }
+                new GoalTypeVM() { Id = GoalTypeEnum.Absolute, Name = "Absolute" },
+                new GoalTypeVM() { Id = GoalTypeEnum.Percent, Name = "Percent" }
             };
             return goalTypes;
         }
 
-        private List<MetricTypeVM> GetMetricTypes()
+        private List<MetricVM> GetMetrics()
         {
-            var metricTypes = new List<MetricTypeVM> {
-                new MetricTypeVM() { Id = 1, Name = "Conversions" },
-                new MetricTypeVM() { Id = 2, Name = "Revenue"}
+            var metrics = new List<MetricVM> {
+                new MetricVM() { Id = MetricEnum.Conversions, Name = "Conversions" },
+                new MetricVM() { Id = MetricEnum.Revenue, Name = "Revenue"}
             };
-            return metricTypes;
+            return metrics;
         }
 
-*/
     }
 }
--- 3/ClientPortal.Web/ClientPortal.Web/Models/GoalVM.cs
+++ 3/ClientPortal.Web/ClientPortal.Web/Models/GoalVM.cs
@@ -7,32 +7,32 @@ namespace ClientPortal.Web.Models
         [Required]
         public string Name { get; set; }
 
-        [Required]
-        public GoalTypeEnum Type { get; set; }
+        public GoalTypeVM Type { get; set; }
+        public GoalTypeEnum TypeId { get; set; }
 
         [Required]
         public string Offer { get; set; }
 
-        [Required]
-        public MetricTypeEnum Metric { get; set; }
+        public MetricVM Metric { get; set; }
+        public MetricEnum MetricId { get; set; }
 
         [Required]
         public decimal Target { get; set; }
     }
 
-    public enum GoalTypeEnum { Absolute, Percent };
-    public enum MetricTypeEnum { Conversions, Revenue };
-/*
+    public enum GoalTypeEnum { Absolute = 1, Percent };
+    public enum MetricEnum { Conversions = 1, Revenue };
+
     public class GoalTypeVM
     {
-        public int Id { get; set; }
+        public GoalTypeEnum Id { get; set; }
         public string Name { get; set; }
     }
 
-    public class MetricTypeVM
+    public class MetricVM
     {
-        public int Id { get; set; }
+        public MetricEnum Id { get; set; }
         public string Name { get; set; }
     }
-*/
+
 }
