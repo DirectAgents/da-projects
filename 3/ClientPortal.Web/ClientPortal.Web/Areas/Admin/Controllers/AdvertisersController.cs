575a5a3fc2e7a982c3a8a92123bc8e7fc9c7f65c
Kevin Slesinsky <kevin@directagents.com>
2013-05-23 12:14:32 -0400

    admin v1

---
+++ 3/ClientPortal.Web/ClientPortal.Web/Areas/Admin/Controllers/AdvertisersController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientPortal.Data.Contexts;
+
+namespace ClientPortal.Web.Areas.Admin.Controllers
+{
+    public class AdvertisersController : Controller
+    {
+        private ClientPortalContext db = new ClientPortalContext();
+
+        //
+        // GET: /Admin/Advertisers/
+
+        public ActionResult Index()
+        {
+            return View(db.Advertisers.ToList());
+        }
+
+        //
+        // GET: /Admin/Advertisers/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Advertiser advertiser = db.Advertisers.Find(id);
+            if (advertiser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertiser);
+        }
+
+        //
+        // GET: /Admin/Advertisers/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Advertisers/Create
+
+        [HttpPost]
+        public ActionResult Create(Advertiser advertiser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Advertisers.Add(advertiser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(advertiser);
+        }
+
+        //
+        // GET: /Admin/Advertisers/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Advertiser advertiser = db.Advertisers.Find(id);
+            if (advertiser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertiser);
+        }
+
+        //
+        // POST: /Admin/Advertisers/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Advertiser advertiser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(advertiser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(advertiser);
+        }
+
+        //
+        // GET: /Admin/Advertisers/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Advertiser advertiser = db.Advertisers.Find(id);
+            if (advertiser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(advertiser);
+        }
+
+        //
+        // POST: /Admin/Advertisers/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Advertiser advertiser = db.Advertisers.Find(id);
+            db.Advertisers.Remove(advertiser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ 3/ClientPortal.Web/ClientPortal.Web/Areas/Admin/Controllers/ContactsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ClientPortal.Data.Contexts;
+
+namespace ClientPortal.Web.Areas.Admin.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private ClientPortalContext db = new ClientPortalContext();
+
+        //
+        // GET: /Admin/Contacts/
+
+        public ActionResult Index()
+        {
+            return View(db.Contacts.ToList());
+        }
+
+        //
+        // GET: /Admin/Contacts/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /Admin/Contacts/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Contacts/Create
+
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contacts.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        //
+        // GET: /Admin/Contacts/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // POST: /Admin/Contacts/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /Admin/Contacts/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Contact contact = db.Contacts.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // POST: /Admin/Contacts/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Contacts.Find(id);
+            db.Contacts.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
