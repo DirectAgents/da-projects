6eea150dba290674784f98101c2c900ff103c325
Aaron Anodide <greentunes@gmail.com>
2013-07-18 12:55:07 -0700

    experimental GUI for cake extracter

---
+++ 3/LineCommander/MainForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace LineCommander
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.commandsDataGridView = new System.Windows.Forms.DataGridView();
+            this.commandsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new LineCommander.DataSet1();
+            this.commandParametersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.commandParametersDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.commandsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commandsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commandParametersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commandParametersDataGridView)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.commandsDataGridView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.commandParametersDataGridView);
+            this.splitContainer1.Size = new System.Drawing.Size(1064, 581);
+            this.splitContainer1.SplitterDistance = 354;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // commandsDataGridView
+            // 
+            this.commandsDataGridView.AllowUserToAddRows = false;
+            this.commandsDataGridView.AllowUserToDeleteRows = false;
+            this.commandsDataGridView.AllowUserToResizeColumns = false;
+            this.commandsDataGridView.AllowUserToResizeRows = false;
+            this.commandsDataGridView.AutoGenerateColumns = false;
+            this.commandsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.commandsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1});
+            this.commandsDataGridView.DataSource = this.commandsBindingSource;
+            this.commandsDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.commandsDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.commandsDataGridView.MultiSelect = false;
+            this.commandsDataGridView.Name = "commandsDataGridView";
+            this.commandsDataGridView.ReadOnly = true;
+            this.commandsDataGridView.RowHeadersVisible = false;
+            this.commandsDataGridView.Size = new System.Drawing.Size(354, 581);
+            this.commandsDataGridView.TabIndex = 0;
+            // 
+            // commandsBindingSource
+            // 
+            this.commandsBindingSource.DataMember = "Commands";
+            this.commandsBindingSource.DataSource = this.dataSet1;
+            this.commandsBindingSource.CurrentChanged += new System.EventHandler(this.commandsBindingSource_CurrentChanged);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "DataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // commandParametersBindingSource
+            // 
+            this.commandParametersBindingSource.DataMember = "Commands_CommandParameters";
+            this.commandParametersBindingSource.DataSource = this.commandsBindingSource;
+            // 
+            // commandParametersDataGridView
+            // 
+            this.commandParametersDataGridView.AllowUserToAddRows = false;
+            this.commandParametersDataGridView.AllowUserToDeleteRows = false;
+            this.commandParametersDataGridView.AllowUserToResizeColumns = false;
+            this.commandParametersDataGridView.AllowUserToResizeRows = false;
+            this.commandParametersDataGridView.AutoGenerateColumns = false;
+            this.commandParametersDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.commandParametersDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.commandParametersDataGridView.DataSource = this.commandParametersBindingSource;
+            this.commandParametersDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.commandParametersDataGridView.Location = new System.Drawing.Point(0, 0);
+            this.commandParametersDataGridView.MultiSelect = false;
+            this.commandParametersDataGridView.Name = "commandParametersDataGridView";
+            this.commandParametersDataGridView.RowHeadersVisible = false;
+            this.commandParametersDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.commandParametersDataGridView.Size = new System.Drawing.Size(706, 581);
+            this.commandParametersDataGridView.TabIndex = 0;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "CommandName";
+            this.dataGridViewTextBoxColumn1.HeaderText = "CommandName";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "CommandName";
+            this.dataGridViewTextBoxColumn2.HeaderText = "CommandName";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "ParameterName";
+            this.dataGridViewTextBoxColumn3.HeaderText = "ParameterName";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "ParameterValue";
+            this.dataGridViewTextBoxColumn4.HeaderText = "ParameterValue";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1064, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(32, 22);
+            this.toolStripButton1.Text = "Run";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 581);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Line Commander";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.commandsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commandsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commandParametersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.commandParametersDataGridView)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource commandsBindingSource;
+        private DataSet1 dataSet1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView commandsDataGridView;
+        private System.Windows.Forms.DataGridView commandParametersDataGridView;
+        private System.Windows.Forms.BindingSource commandParametersBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+
+
+    }
+}
+
---
+++ 3/LineCommander/MainForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace LineCommander
+{
+    public partial class MainForm : Form
+    {
+        private bool selectionEnabled;
+
+        public MainForm()
+        {
+            var catalog = new DirectoryCatalog(@".", "CakeExtracter.exe");
+            var composistionContainer = new CompositionContainer(catalog);
+            composistionContainer.ComposeParts(this);
+
+            InitializeComponent();
+
+            foreach (var consoleCommand in this.Commands)
+            {
+                var commandRow = dataSet1.Commands.AddCommandsRow(consoleCommand.Command);
+                foreach (var item in consoleCommand.GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance))
+                {
+                    var commandParametersRow = dataSet1.CommandParameters.AddCommandParametersRow(commandRow, item.Name, "");
+                }
+            }
+
+            this.Shown += Form1_Shown;
+        }
+
+        void Form1_Shown(object sender, EventArgs e)
+        {
+            this.commandsDataGridView.ClearSelection();
+            this.selectionEnabled = true;
+        }
+
+        [ImportMany]
+        public IEnumerable<CakeExtracter.Common.ConsoleCommand> Commands { get; set; }
+
+        private void commandsBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
