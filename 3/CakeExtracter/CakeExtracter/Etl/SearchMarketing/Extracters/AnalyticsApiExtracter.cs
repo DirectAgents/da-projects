0b84bc65e217ab6497dc41b107e162ca14f19827
Kevin Slesinsky <kevin@directagents.com>
2013-10-18 12:30:20 -0400

    auth work for analytics

--- 3/CakeExtracter/CakeExtracter/Etl/SearchMarketing/Extracters/AnalyticsApiExtracter.cs
+++ 3/CakeExtracter/CakeExtracter/Etl/SearchMarketing/Extracters/AnalyticsApiExtracter.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Analytics.v3;
+using Google.Apis.Services;
+using Google.Apis.Authentication.OAuth2.DotNetOpenAuth;
+using Google.Apis.Authentication.OAuth2;
+using DotNetOpenAuth.OAuth2;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CakeExtracter.Etl.SearchMarketing.Extracters
 {
@@ -21,7 +27,29 @@ namespace CakeExtracter.Etl.SearchMarketing.Extracters
 
         protected override void Extract()
         {
-            throw new NotImplementedException();
+            string certPath = "";
+            var certificate = new X509Certificate2(certPath, "notasecret", X509KeyStorageFlags.Exportable);
+            var provider = new AssertionFlowClient(GoogleAuthenticationServer.Description, certificate)
+            {
+                ServiceAccountId = "960492209499-cdktdkluj4sj5ee8bto7hdi7m058lfg9@developer.gserviceaccount.com",
+                //Scope = DriveService
+            };
+
+            //var provider = new NativeApplicationClient(GoogleAuthenticationServer.Description)
+            //{
+            //    ClientIdentifier = "",
+            //    ClientSecret = ""
+            //};
+            //var auth = new OAuth2Authenticator<NativeApplicationClient>(provider, GetAuthorization);
+            //var init = new BaseClientService.Initializer() {
+            //var service = new AnalyticsService();
         }
+
+        private static IAuthorizationState GetAuthorization(NativeApplicationClient arg)
+        {
+            //arg.RefreshToken(
+            return null;
+        }
+
     }
 }
