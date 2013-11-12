5a6d1092679e82048298951c5f4e2e231e28cee2
Kevin Slesinsky <kevin@directagents.com>
2013-11-12 11:22:39 -0500

    updating adwords cmd & loader to use searchaccounts

--- 3/CakeExtracter/CakeExtracter/Commands/SynchSearchDailySummariesAdWordsCommand.cs
+++ 3/CakeExtracter/CakeExtracter/Commands/SynchSearchDailySummariesAdWordsCommand.cs
@@ -13,14 +13,14 @@ namespace CakeExtracter.Commands
     public class SynchSearchDailySummariesAdWordsCommand : ConsoleCommand
     {
         public int AdvertiserId { get; set; }
-        public string ClientCustomerId { get; set; }
+        public string ClientId { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
 
         public override void ResetProperties()
         {
             AdvertiserId = 0;
-            ClientCustomerId = null;
+            ClientId = null;
             StartDate = null;
             EndDate = null;
         }
@@ -28,8 +28,8 @@ namespace CakeExtracter.Commands
         public SynchSearchDailySummariesAdWordsCommand()
         {
             IsCommand("synchSearchDailySummariesAdWords", "synch SearchDailySummaries for AdWords");
-            HasOption<int>("a|advertiserId=", "Advertiser Id (default is 0, meaning all search advertisers, unless Client Customer Id specified)", c => AdvertiserId = c);
-            HasOption<string>("v|clientCustomerId=", "Client Customer Id", c => ClientCustomerId = c);
+            HasOption<int>("a|advertiserId=", "Advertiser Id (default is 0, meaning all search advertisers, unless Client Id specified)", c => AdvertiserId = c);
+            HasOption<string>("v|clientId=", "Client Id", c => ClientId = c);
             HasOption<DateTime>("s|startDate=", "Start Date (default is one month ago)", c => StartDate = c);
             HasOption<DateTime>("e|endDate=", "End Date (default is yesterday)", c => EndDate = c);
         }
@@ -40,10 +40,10 @@ namespace CakeExtracter.Commands
             var yesterday = DateTime.Today.AddDays(-1);
             var dateRange = new DateRange(StartDate ?? oneMonthAgo, EndDate ?? yesterday);
 
-            foreach (var clientId in GetClientCustomerIds(this.AdvertiserId, this.ClientCustomerId))
+            foreach (var searchAccount in GetSearchAccounts())
             {
-                var extracter = new AdWordsApiExtracter(clientId, dateRange);
-                var loader = new AdWordsApiLoader();
+                var extracter = new AdWordsApiExtracter(searchAccount.AccountCode, dateRange);
+                var loader = new AdWordsApiLoader(searchAccount.SearchAccountId);
                 var extracterThread = extracter.Start();
                 var loaderThread = loader.Start(extracter);
                 extracterThread.Join();
@@ -52,43 +52,52 @@ namespace CakeExtracter.Commands
             return 0;
         }
 
-        internal static IEnumerable<string> GetClientCustomerIds(int advertiserId, string clientCustomerId)
+        public IEnumerable<SearchAccount> GetSearchAccounts()
         {
-            List<string> clientIds = new List<string>();
-            if (advertiserId != 0)
-            {
-                clientIds.Add(GetClientIdFromAdvertiserId(advertiserId));
-            }
-            else if (string.IsNullOrWhiteSpace(clientCustomerId))
-            {   // no advertiserid or adwords accountid specified
-                // get all adwords ids that exist in the advertisers table
-                using (var db = new ClientPortalContext())
-                {
-                    var ids = db.Advertisers.AsQueryable().Where(a => a.AdWordsAccountId != null)
-                                .Select(a => a.AdWordsAccountId).ToList();
-                    clientIds.AddRange(ids);
-                }
-            }
-
-            if (!string.IsNullOrWhiteSpace(clientCustomerId))
-                clientIds.Add(clientCustomerId);
-
-            return clientIds;
-        }
-
-        internal static string GetClientIdFromAdvertiserId(int advertiserId)
-        {
-            Logger.Info("Getting AdWords id for advertiser id {0}", advertiserId);
+            var searchAccounts = new List<SearchAccount>();
 
             using (var db = new ClientPortalContext())
             {
-                var idString = db.Advertisers.Find(advertiserId).AdWordsAccountId;
+                if (this.ClientId == null)
+                {
+                    if (this.AdvertiserId > 0)
+                    {   // No ClientId specified; get all SearchAccounts for this Advertiser
+                        searchAccounts = db.SearchAccounts.Where(sa => sa.AdvertiserId == AdvertiserId).ToList();
+                    }
+                }
+                else // ClientId specified
+                {
+                    var searchAccount = db.SearchAccounts.SingleOrDefault(sa => sa.AccountCode == ClientId && sa.Channel == "google");
+                    if (searchAccount != null)
+                    {
+                        if (AdvertiserId > 0 && searchAccount.AdvertiserId != AdvertiserId)
+                            Logger.Info("AdvertiserId does not match that of SearchAccount specified by ClientId");
 
-                if (string.IsNullOrWhiteSpace(idString))
-                    throw new Exception(string.Format("No AdWords id is set for advertiser id {0}", advertiserId));
-
-                return idString;
+                        searchAccounts.Add(searchAccount);
+                    }
+                    else // didn't find a matching SearchAccount; see about creating a new one
+                    {
+                        if (AdvertiserId > 0)
+                        {
+                            searchAccount = new SearchAccount()
+                            {
+                                AdvertiserId = this.AdvertiserId,
+                                Channel = "google",
+                                AccountCode = ClientId
+                                // to fill in later: Name, ExternalId
+                            };
+                            db.SearchAccounts.Add(searchAccount);
+                            db.SaveChanges();
+                            searchAccounts.Add(searchAccount); // Q: is the id filled in?
+                        }
+                        else
+                        {
+                            Logger.Info("SearchAccount with AccountCode {0} not found and no AdvertiserId specified", ClientId);
+                        }
+                    }
+                }
             }
+            return searchAccounts;
         }
 
     }
