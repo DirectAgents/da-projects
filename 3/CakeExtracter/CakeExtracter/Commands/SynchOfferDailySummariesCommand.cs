1bba77a3f465543fe5ee167edb92108b97f20ee6
Kevin Slesinsky <kevin@directagents.com>
2014-04-09 15:19:24 -0400

    SynchOfferDailySummaries

---
+++ 3/CakeExtracter/CakeExtracter/Commands/SynchOfferDailySummariesCommand.cs
@@ -0,0 +1,97 @@
+﻿using CakeExtracter.Common;
+using CakeExtracter.Etl.CakeMarketing.Extracters;
+using CakeExtracter.Etl.CakeMarketing.Loaders;
+using ClientPortal.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+
+namespace CakeExtracter.Commands
+{
+    [Export(typeof(ConsoleCommand))]
+    public class SynchOfferDailySummariesCommand : ConsoleCommand
+    {
+        public string Advertiser { get; set; }
+        public int? OfferId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public override void ResetProperties()
+        {
+            Advertiser = null;
+            OfferId = null;
+            StartDate = null;
+            EndDate = null;
+        }
+
+        public SynchOfferDailySummariesCommand()
+        {
+            RunBefore(new SynchAdvertisersCommand());
+            RunBefore(new SynchOffersCommand());
+
+            IsCommand("synchOfferDailySummaries", "synch OfferDailySummaries for an advertisers offers in a date range");
+            HasOption("a|advertiserId=", "Advertiser Id (* = all advertisers)", c => Advertiser = c);
+            HasOption<int>("o|offerId=", "Offer Id (default = all)", c => OfferId = c);
+            HasOption("s|startDate=", "Start Date (default is two months ago)", c => StartDate = DateTime.Parse(c));
+            HasOption("e|endDate=", "End Date (default is today)", c => EndDate = DateTime.Parse(c));
+        }
+
+        public override int Execute(string[] remainingArguments)
+        {
+            var twoMonthAgo = DateTime.Today.AddMonths(-2);
+            var dateRange = new DateRange(StartDate ?? twoMonthAgo, EndDate ?? DateTime.Today);
+
+            dateRange.ToDate = dateRange.ToDate.AddDays(1); // cake requires the date _after_ the last date you want stats for
+
+            foreach (var advertiserId in GetAdvertiserIds())
+            {
+                var extracter = new OfferDailySummariesExtracter(dateRange, advertiserId, OfferId);
+                var loader = new OfferDailySummariesLoader();
+                var extracterThread = extracter.Start();
+                var loaderThread = loader.Start(extracter);
+                extracterThread.Join();
+                loaderThread.Join();
+
+                // Set "LatestDaySums" datetime (if we're updating all offers to today)
+                if (dateRange.ToDate > DateTime.Today && !OfferId.HasValue)
+                {
+                    using (var db = new ClientPortalContext())
+                    {
+                        var advertiser = db.Advertisers.Where(a => a.AdvertiserId == advertiserId).FirstOrDefault();
+                        if (advertiser != null)
+                        {
+                            advertiser.LatestDaySums = DateTime.Now;
+                            db.SaveChanges();
+                        }
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private IEnumerable<int> GetAdvertiserIds()
+        {
+            if (string.IsNullOrWhiteSpace(Advertiser) || Advertiser == "*")
+            {
+                List<int> advertiserIds;
+                using (var db = new ClientPortalContext())
+                {
+                    advertiserIds = db.Advertisers
+                                      .OrderBy(c => c.AdvertiserId)
+                                      .Select(c => c.AdvertiserId)
+                                      .ToList();
+                }
+                foreach (var advertiserId in advertiserIds)
+                {
+                    yield return advertiserId;
+                }
+            }
+            else
+            {
+                yield return int.Parse(Advertiser);
+            }
+        }
+
+    }
+}
---
+++ 3/CakeExtracter/CakeExtracter/Etl/CakeMarketing/Extracters/OfferDailySummariesExtracter.cs
@@ -0,0 +1,63 @@
+﻿using CakeExtracter.CakeMarketingApi;
+using CakeExtracter.CakeMarketingApi.Entities;
+using CakeExtracter.Common;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CakeExtracter.Etl.CakeMarketing.Extracters
+{
+    public class OfferDailySummariesExtracter : Extracter<OfferDailySummary>
+    {
+        private readonly DateRange dateRange;
+        private readonly int advertiserId;
+        private readonly int? offerId;
+
+        public OfferDailySummariesExtracter(DateRange dateRange, int advertiserId, int? offerId)
+        {
+            this.dateRange = dateRange;
+            this.advertiserId = advertiserId;
+            this.offerId = offerId;
+        }
+
+        protected override void Extract()
+        {
+            Logger.Info("Getting offerIds for advertiserId={0}", advertiserId);
+
+            List<int> offerIds;
+            if (this.offerId.HasValue)
+            {
+                offerIds = new List<int> { offerId.Value };
+            }
+            else
+            {
+                offerIds = CakeMarketingUtility.OfferIds(advertiserId);
+            }
+
+            Logger.Info("Extracting DailySummaries for {0} offerIds between {2} and {3}: {1}",
+                            offerIds.Count,
+                            string.Join(", ", offerIds),
+                            dateRange.FromDate.ToShortDateString(),
+                            dateRange.ToDate.ToShortDateString());
+
+            Parallel.ForEach(offerIds, offerId =>
+            {
+                var dailySummaries = CakeMarketingUtility.DailySummaries(dateRange, advertiserId, offerId, 0, 0);
+
+                Logger.Info("Extracted {0} DailySummaries for offerId={1}", dailySummaries.Count, offerId);
+
+                Add(dailySummaries.Select(c => new OfferDailySummary(offerId, c)));
+
+                // Check for dates with no data
+                var datesExtracted = dailySummaries.Select(ds => ds.Date);
+                for (var iDate = dateRange.FromDate; iDate < dateRange.ToDate; iDate = iDate.AddDays(1))
+                {
+                    if (!datesExtracted.Contains(iDate))
+                        Add(new OfferDailySummary(offerId, iDate));
+                }
+            });
+
+            End();
+        }
+    }
+}
