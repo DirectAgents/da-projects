18a4c6bdcf3af30cf9cb47ca2810459e594fd0ba
Kevin Slesinsky <kevin@directagents.com>
2014-05-29 12:55:25 -0400

    projects- DADomain & CakeExtracter

--- 3/DirectAgents.Web/DirectAgents.Web/Controllers/BudgetsController.cs
+++ 3/DirectAgents.Web/DirectAgents.Web/Controllers/BudgetsController.cs
@@ -1,17 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using DirectAgents.Domain.Abstract;
+using DirectAgents.Domain.Concrete;
+using DirectAgents.Domain.Contexts;
 using System.Web.Mvc;
 
 namespace DirectAgents.Web.Controllers
 {
     public class BudgetsController : Controller
     {
+        private IMainRepository mainRepo;
+
+        public BudgetsController()
+        {
+            this.mainRepo = new MainRepository(new DAContext());
+        }
+
+        public ActionResult Advertisers()
+        {
+            var advertisers = mainRepo.GetAdvertisers();
+
+            return View(advertisers);
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
+        protected override void Dispose(bool disposing)
+        {
+            mainRepo.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
