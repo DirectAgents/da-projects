f14b5925ce2608d6d51f3a511d5332a969754ce7
Kevin Slesinsky <kevin@directagents.com>
2014-05-30 18:08:00 -0400

    show & synch

--- 3/DirectAgents.Web/DirectAgents.Web/Controllers/BudgetsController.cs
+++ 3/DirectAgents.Web/DirectAgents.Web/Controllers/BudgetsController.cs
@@ -1,6 +1,9 @@
-﻿using DirectAgents.Domain.Abstract;
+﻿using CakeExtracter.Commands;
+using DirectAgents.Domain.Abstract;
 using DirectAgents.Domain.Concrete;
 using DirectAgents.Domain.Contexts;
+using DirectAgents.Domain.Entities.Cake;
+using DirectAgents.Web.Models;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -18,13 +21,99 @@ namespace DirectAgents.Web.Controllers
         public ActionResult Advertisers()
         {
             var advertisers = mainRepo.GetAdvertisers();
-
             return View(advertisers.OrderBy(a => a.AdvertiserName));
         }
 
-        public ActionResult Index()
+        public ActionResult AdvertiserRow(int advId)
         {
-            return View();
+            var advertiser = mainRepo.GetAdvertiser(advId);
+            if (advertiser == null)
+                return null;
+            return PartialView(advertiser);
+        }
+
+        public ActionResult Offers(int? advId)
+        {
+            var offers = mainRepo.GetOffers(advId);
+            return View(offers.OrderBy(o => o.OfferId));
+        }
+
+        public ActionResult Show(int offerId)
+        {
+            var offer = mainRepo.GetOffer(offerId);
+            if (offer == null)
+                return Content("Offer not found");
+
+            var ods = mainRepo.GetOfferDailySummaries(offerId);
+
+            var model = new OfferVM(offer, ods);
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int offerId)
+        {
+            var offer = mainRepo.GetOffer(offerId);
+            if (offer == null)
+                return Content("Offer not found");
+
+            return View(offer);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Offer inOffer)
+        {
+            var offer = mainRepo.GetOffer(inOffer.OfferId);
+            if (offer == null)
+                return Content("Offer not found");
+
+            offer.Budget = inOffer.Budget;
+            offer.BudgetIsMonthly = inOffer.BudgetIsMonthly;
+            offer.BudgetStart = inOffer.BudgetStart;
+            mainRepo.SaveChanges();
+
+            return RedirectToAction("Show", new { offerId = inOffer.OfferId });
+        }
+
+        public ActionResult SynchStats(int offerId)
+        {
+            var offer = mainRepo.GetOffer(offerId);
+            if (offer == null)
+                return Content("Offer not found");
+
+            DASynchOfferDailySummariesCommand.RunStatic(0, offerId, offer.DateCreated.Date);
+
+            return RedirectToAction("Show", new { offerId = offerId });
+        }
+
+        // --- AJAX actions ---
+
+        public JsonResult SynchOffers(int advId)
+        {
+            DASynchOffersCommand.RunStatic(advId);
+            return null;
+        }
+
+        //// Set budget to 0 (only if it is null)
+        //public JsonResult Initialize(int offerId)
+        //{
+        //    var offer = mainRepo.GetOffer(offerId);
+        //    if (offer != null && !offer.Budget.HasValue)
+        //    {
+        //        offer.Budget = 0;
+        //        mainRepo.SaveChanges();
+        //    }
+        //    return null;
+        //}
+
+        //---
+
+        public ActionResult Setup()
+        {
+            //DASynchAdvertisersCommand.RunStatic(0);
+            DASynchOffersCommand.RunStatic(278);
+
+            return Content("ok");
         }
 
         protected override void Dispose(bool disposing)
---
+++ 3/DirectAgents.Web/DirectAgents.Web/Models/OfferVM.cs
@@ -0,0 +1,73 @@
+﻿using DirectAgents.Domain.Entities.Cake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DirectAgents.Web.Models
+{
+    public class OfferVM
+    {
+        public OfferVM(Offer offer, IQueryable<OfferDailySummary> offerDailySummaries)
+        {
+            this.Offer = offer;
+            this.OfferDailySummaries = offerDailySummaries.OrderBy(o => o.Date).AsEnumerable();
+        }
+
+        public Offer Offer { get; set; }
+        public IEnumerable<OfferDailySummary> OfferDailySummaries { get; set; }
+
+        public int Views
+        {
+            get { return OfferDailySummaries.Sum(o => o.Views); }
+        }
+        public int Clicks
+        {
+            get { return OfferDailySummaries.Sum(o => o.Clicks); }
+        }
+        public int Conversions
+        {
+            get { return OfferDailySummaries.Sum(o => o.Conversions); }
+        }
+        public int Paid
+        {
+            get { return OfferDailySummaries.Sum(o => o.Paid); }
+        }
+        public int Sellable
+        {
+            get { return OfferDailySummaries.Sum(o => o.Sellable); }
+        }
+        public decimal Revenue
+        {
+            get { return OfferDailySummaries.Sum(o => o.Revenue); }
+        }
+        public decimal Cost
+        {
+            get { return OfferDailySummaries.Sum(o => o.Cost); }
+        }
+
+        public DateTime? EarliestStatDate
+        {
+            get
+            {
+                var ods = OfferDailySummaries.FirstOrDefault();
+                if (ods == null)
+                    return null;
+                else
+                    return ods.Date;
+            }
+        }
+
+        public DateTime? LatestStatDate
+        {
+            get
+            {
+                var ods = OfferDailySummaries.LastOrDefault();
+                if (ods == null)
+                    return null;
+                else
+                    return ods.Date;
+            }
+        }
+    }
+}
