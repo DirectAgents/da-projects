7632848b7e664cc837c6ac0057900adc751d7a6b
Kevin Slesinsky <kevin@directagents.com>
2014-06-23 15:20:00 -0400

    refactor Eom.CheckFinalizationMargins

--- 1/EomTool/EomApp1/Screens/Final/Models/Eom.cs
+++ 1/EomTool/EomApp1/Screens/Final/Models/Eom.cs
@@ -88,30 +88,24 @@ namespace EomApp1.Screens.Final.Models
                         if (row.MarginExempt && !affIdsRequireNegativeApproval.Contains(row.AffId))
                             continue;
 
+                        bool approvalNeeded = false;
                         if (row.TotalRevenue != 0)
                         {
                             var revUSD = row.RevToUSD * row.TotalRevenue;
                             var costUSD = row.CostToUSD * row.TotalCost;
                             var marginPct = (1 - costUSD / revUSD);
-                            bool approvalNeeded = (marginPct < minimumMarginPct || marginPct <= 0 || marginPct >= 1);
+                            approvalNeeded = (marginPct < minimumMarginPct || marginPct <= 0 || marginPct >= 1);
 
                             if (row.MarginExempt && affIdsRequireNegativeApproval.Contains(row.AffId) && marginPct >= 0)
                                 approvalNeeded = false; // special cases - only exempt from negative margin approval
-
-                            if (approvalNeeded)
-                            {
-                                // Needs approval; see if there's an unused marginApproval. if so, set it to used (and don't add to rejectedAffIdList)
-                                decimal marginPctRounded = decimal.Round(marginPct, 3);
-                                var marginApproval = db.MarginApprovals.Where(ma => ma.pid == pid && ma.affid == row.AffId && ma.margin_pct == marginPctRounded
-                                                                                    && !ma.used.HasValue).FirstOrDefault();
-                                if (marginApproval != null)
-                                    marginApproval.used = now;
-                                else
-                                    rejectedAffIdList.Add(row.AffId);
-                            }
                         }
                         else if (row.TotalCost != 0 && (!row.MarginExempt || affIdsRequireNegativeApproval.Contains(row.AffId)))
-                        { // revenue is 0; cost is not 0; requires approval
+                        {
+                            approvalNeeded = true; // revenue is 0; cost is not 0; requires approval
+                        }
+
+                        if (approvalNeeded)
+                        {  // See if there's an unused marginApproval. if so, set it to used (and don't add to rejectedAffIdList)
                             var marginApproval = db.MarginApprovals.Where(ma => ma.pid == pid && ma.affid == row.AffId && !ma.used.HasValue &&
                                                                           ma.RevenueCurrency.name == row.RevenueCurrency.name && ma.total_revenue == row.TotalRevenue &&
                                                                           ma.CostCurrency.name == row.CostCurrency.name && ma.total_cost == row.TotalCost).FirstOrDefault();
