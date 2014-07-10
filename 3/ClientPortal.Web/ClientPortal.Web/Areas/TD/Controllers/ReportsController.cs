6a5f77b6c9df29b36b06a4e9b0569af642b4652b
Kevin Slesinsky <kevin@directagents.com>
2014-07-10 17:59:11 -0400

    Reports/ChartSetup

--- 3/ClientPortal.Web/ClientPortal.Web/Areas/TD/Controllers/ReportsController.cs
+++ 3/ClientPortal.Web/ClientPortal.Web/Areas/TD/Controllers/ReportsController.cs
@@ -1,5 +1,6 @@
 ﻿using ClientPortal.Data.Contracts;
 using ClientPortal.Data.Entities.TD;
+using ClientPortal.Web.Areas.TD.Models;
 using DirectAgents.Mvc.KendoGridBinder;
 using System;
 using System.Collections.Generic;
@@ -18,21 +19,42 @@ namespace ClientPortal.Web.Areas.TD.Controllers
             tdRepo = tdRepository;
         }
 
-        public ActionResult Summary()
+        public ActionResult Sample()
         {
             return PartialView();
         }
 
-        public ActionResult Sample()
+        public ActionResult Summary(string metric1, string metric2)
         {
-            return PartialView();
+            if (String.IsNullOrWhiteSpace(metric1))
+            {
+                return PartialView("ChartSetup");
+            }
+
+            var metricsList = new List<string> { metric1 };
+            if (!String.IsNullOrWhiteSpace(metric2))
+                metricsList.Add(metric2);
+            var model = new ReportModel
+            {
+                Metrics = metricsList.ToArray()
+            };
+            return PartialView(model);
         }
 
         public JsonResult SampleData(KendoGridRequest request)
         {
             var summaries = tdRepo.GetDailySummaries(null, null, null);
             var kgrid = new KendoGrid<DailySummary>(request, summaries);
-
+            if (summaries.Any())
+            {
+                kgrid.aggregates = new
+                {
+                    Impressions = new { sum = summaries.Sum(s => s.Impressions) },
+                    Clicks = new { sum = summaries.Sum(s => s.Clicks) },
+                    Conversions = new { sum = summaries.Sum(s => s.Conversions) },
+                    Revenue = new { sum = summaries.Sum(s => s.Revenue) }
+                };
+            }
             var json = Json(kgrid, JsonRequestBehavior.AllowGet);
             return json;
         }
---
+++ 3/ClientPortal.Web/ClientPortal.Web/Areas/TD/Models/ReportModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientPortal.Web.Areas.TD.Models
+{
+    public class ReportModel
+    {
+        public string[] Metrics { get; set; }
+    }
+}
