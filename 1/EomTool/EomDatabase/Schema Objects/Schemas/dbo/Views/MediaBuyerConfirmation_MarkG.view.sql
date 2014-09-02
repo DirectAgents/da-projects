f659be47424db592ab0ded5c00e23fad16ace7b5
Kevin Slesinsky <kevin@directagents.com>
2014-09-02 16:16:01 -0400

    MediaBuyerConfirmation views

---
+++ 1/EomTool/EomDatabase/Schema Objects/Schemas/dbo/Views/MediaBuyerConfirmation_MarkG.view.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[MediaBuyerConfirmation_MarkG] AS
+SELECT
+REPLACE(DB_NAME(),'DADatabase','') as Period,
+a.name2 as Publisher,
+m.name as [Media Buyer],
+'' as [Correct Media Buyer]
+FROM affiliate a JOIN mediabuyer m on m.id=a.media_buyer_id
+WHERE m.id not in (9,33,50,22,27,10,1,15,44,11,45,3,38,52,41,55)
---
+++ 1/EomTool/EomDatabase/Schema Objects/Schemas/dbo/Views/MediaBuyerConfirmation_Megan.view.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dbo].[MediaBuyerConfirmation_Megan] AS
+SELECT
+REPLACE(DB_NAME(),'DADatabase','') as Period,
+a.name2 as Publisher,
+m.name as [Media Buyer],
+'' as [Correct Media Buyer]
+FROM affiliate a JOIN mediabuyer m on m.id=a.media_buyer_id
+WHERE m.id in (9,33)
