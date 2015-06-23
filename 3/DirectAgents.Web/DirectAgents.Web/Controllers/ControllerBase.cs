c4c9f6199e27c718f281e8652c03e58624516d9d
Kevin Slesinsky <kevin@directagents.com>
2015-06-23 18:17:54 -0400

    DAWeb- misc updates

--- 3/DirectAgents.Web/DirectAgents.Web/Controllers/ControllerBase.cs
+++ 3/DirectAgents.Web/DirectAgents.Web/Controllers/ControllerBase.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
 using DirectAgents.Domain.Abstract;
 
 namespace DirectAgents.Web.Controllers
@@ -15,6 +18,51 @@ namespace DirectAgents.Web.Controllers
         {
             mainRepo.Dispose();
             base.Dispose(disposing);
+        }
+    }
+
+    public class JsonpResult : System.Web.Mvc.JsonResult
+    {
+        public override void ExecuteResult(ControllerContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            HttpResponseBase response = context.HttpContext.Response;
+
+            if (!String.IsNullOrEmpty(ContentType))
+            {
+                response.ContentType = ContentType;
+            }
+            else
+            {
+                response.ContentType = "application/javascript";
+            }
+            if (ContentEncoding != null)
+            {
+                response.ContentEncoding = ContentEncoding;
+            }
+            if (Data != null)
+            {
+                // The JavaScriptSerializer type was marked as obsolete prior to .NET Framework 3.5 SP1
+#pragma warning disable 0618
+                HttpRequestBase request = context.HttpContext.Request;
+
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                var callbackName = request.Params["jsoncallback"] ?? "mycallback";
+                response.Write(callbackName + "(" + serializer.Serialize(Data) + ")");
+#pragma warning restore 0618
+            }
+        }
+    }
+
+    public static class JsonResultExtensions
+    {
+        public static JsonpResult ToJsonp(this JsonResult json)
+        {
+            return new JsonpResult { ContentEncoding = json.ContentEncoding, ContentType = json.ContentType, Data = json.Data, JsonRequestBehavior = json.JsonRequestBehavior };
         }
     }
 }
