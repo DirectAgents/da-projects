855f7fef201711dc37da0b3f1331942c449a0a00
Kevin Slesinsky <kevin@directagents.com>
2015-08-04 17:57:56 -0400

    DA- AdRollAds migration

--- 1/DirectAgents.Domain/Contexts/TDContext.cs
+++ 1/DirectAgents.Domain/Contexts/TDContext.cs
@@ -21,9 +21,17 @@ namespace DirectAgents.Domain.Contexts
                 .ToTable("AdvertisableStat", adrollSchema);
             modelBuilder.Entity<AdvertisableStat>()
                 .Property(t => t.Cost).HasPrecision(18, 6);
+            modelBuilder.Entity<Ad>().ToTable("Ad", adrollSchema);
+            modelBuilder.Entity<AdDailySummary>()
+                .HasKey(ds => new { ds.Date, ds.AdId })
+                .ToTable("AdDailySummary", adrollSchema);
+            modelBuilder.Entity<AdDailySummary>()
+                .Property(ds => ds.Cost).HasPrecision(18, 6);
         }
 
         public DbSet<Advertisable> Advertisables { get; set; }
         public DbSet<AdvertisableStat> AdvertisableStats { get; set; }
+        public DbSet<Ad> AdRollAds { get; set; }
+        public DbSet<AdDailySummary> AdRollAdDailySummaries { get; set; }
     }
 }
---
+++ 1/DirectAgents.Domain/Entities/AdRoll/AdDailySummary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DirectAgents.Domain.Entities.AdRoll
+{
+    public class AdDailySummary
+    {
+        public DateTime Date { get; set; }
+        public int AdId { get; set; }
+        public virtual Ad Ad { get; set; }
+
+        public int Impressions { get; set; }
+        public int Clicks { get; set; }
+        public int CTC { get; set; }
+        public int VTC { get; set; }
+        public decimal Cost { get; set; }
+        public int Prospects { get; set; }
+    }
+}
