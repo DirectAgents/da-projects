d01e0359eee55616a1e937d7e351943fd9914ff7
Nathaniel <nholder@DirectAgents.local>
2016-01-13 17:01:47 -0500

    fBudgetInfo

--- 3/DirectAgents.Reporting/sql/fBudgetInfo.sql
+++ 3/DirectAgents.Reporting/sql/fBudgetInfo.sql
@@ -3,14 +3,14 @@
 , @StartDate datetime = NULL
 , @EndDate datetime = NULL
 ) RETURNS TABLE AS RETURN
-SELECT ISNULL(td.PlatformBudgetInfo.MediaSpend, ISNULL(td.BudgetInfo.MediaSpend, td.Campaign.MediaSpend)) AS MediaSpend
-	, ISNULL(td.PlatformBudgetInfo.MgmtFeePct, ISNULL(td.BudgetInfo.MgmtFeePct, td.Campaign.MgmtFeePct)) AS MgmtFeePct
-	, ISNULL(td.PlatformBudgetInfo.MarginPct, ISNULL(td.BudgetInfo.MarginPct, td.Campaign.MarginPct)) AS MarginPct
+SELECT ISNULL(PlatformBudgetInfo.MediaSpend, ISNULL(BudgetInfo.MediaSpend, Campaign.MediaSpend)) AS MediaSpend
+	, ISNULL(PlatformBudgetInfo.MgmtFeePct, ISNULL(BudgetInfo.MgmtFeePct, Campaign.MgmtFeePct)) AS MgmtFeePct
+	, ISNULL(PlatformBudgetInfo.MarginPct, ISNULL(BudgetInfo.MarginPct, Campaign.MarginPct)) AS MarginPct
 	, CAST(CASE WHEN Platform.Name = 'Facebook' THEN 0 ELSE 1 END AS bit) AS ShowClickAndViewConv
-	, td.Campaign.Id AS CampaignId
-	, td.Campaign.AdvertiserId
-	, td.Account.Id AS AccountId
-	, td.Account.PlatformId
+	, Campaign.Id AS CampaignId
+	, Campaign.AdvertiserId
+	, Account.Id AS AccountId
+	, Account.PlatformId
 FROM td.Campaign
 LEFT OUTER JOIN td.BudgetInfo
 	ON (BudgetInfo.CampaignId = Campaign.Id)
