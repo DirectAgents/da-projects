e2d66881f98851e79b03a60545c06597c2fc90b9
Kevin Slesinsky <kevin@directagents.com>
2016-02-23 18:19:54 -0500

    Seeder

---
+++ 1/DirectAgents.Domain/Seed/TDSeeder.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq;
+using DirectAgents.Domain.Contexts;
+using DirectAgents.Domain.Entities.TD;
+
+namespace DirectAgents.Domain.Seed
+{
+    public class TDSeeder
+    {
+        // Caller is responsible for instantiating and disposing of the context(s)
+        public DATDContext context { get; set; }
+
+        public TDSeeder(DATDContext context)
+        {
+            this.context = context;
+        }
+
+        public void SeedPlatforms()
+        {
+            AddPlatformIfNotExist("adr", "AdRoll");
+            AddPlatformIfNotExist("dbm", "DBM");
+            AddPlatformIfNotExist("fb", "Facebook");
+            AddPlatformIfNotExist("tw", "Twitter");
+            AddPlatformIfNotExist("insta", "Instagram");
+            AddPlatformIfNotExist("datd", "DA Trading Desk");
+            AddPlatformIfNotExist("mf", "Management Fee");
+            AddPlatformIfNotExist("adel", "Adelphic");
+            AddPlatformIfNotExist("adf", "Adform");
+            AddPlatformIfNotExist("adt", "Adroit");
+            AddPlatformIfNotExist("bc", "Bluecore");
+            AddPlatformIfNotExist("li", "LiveIntent");
+            AddPlatformIfNotExist("tab", "Taboola");
+            AddPlatformIfNotExist("tc", "TapCommerce");
+            AddPlatformIfNotExist("yam", "YAM");
+            AddPlatformIfNotExist("zem", "Zemanta");
+        }
+
+        public void AddPlatformIfNotExist(string code, string name)
+        {
+            if (!context.Platforms.Any(p => p.Code == code))
+            {
+                var platform = new Platform
+                {
+                    Code = code,
+                    Name = name
+                };
+                context.Platforms.Add(platform);
+                context.SaveChanges();
+            }
+        }
+    }
+}
