3dc63839a4a36461ddddbe2d94d5bf498c4c369b
nathaniel-holder <nholder@DirectAgents.local>
2016-03-16 18:30:22 -0400

    Fixed: stats not included that are before the first PlatformBudgetInfo or BudgetInfo date

--- 3/DirectAgents.Reporting/sql/fBudgetInfo.sql
+++ 3/DirectAgents.Reporting/sql/fBudgetInfo.sql
@@ -7,7 +7,9 @@ SELECT ISNULL(PlatformBudgetInfo.MediaSpend, ISNULL(BudgetInfo.MediaSpend, Campa
 	, ISNULL(PlatformBudgetInfo.MgmtFeePct, ISNULL(BudgetInfo.MgmtFeePct, Campaign.MgmtFeePct)) AS MgmtFeePct
 	, ISNULL(PlatformBudgetInfo.MarginPct, ISNULL(BudgetInfo.MarginPct, Campaign.MarginPct)) AS MarginPct
 	, CAST(CASE WHEN Platform.Name = 'Facebook' THEN 0 ELSE 1 END AS bit) AS ShowClickAndViewConv
-	, ISNULL(PlatformBudgetInfo.Date, ISNULL(BudgetInfo.Date, ISNULL(@StartDate, '1/1/1970'))) AS AsOfDate
+	, ISNULL(CASE WHEN PlatformBudgetInfo.Date <= @StartDate THEN PlatformBudgetInfo.Date END,
+	  ISNULL(CASE WHEN BudgetInfo.Date <= @StartDate THEN BudgetInfo.Date END,
+	  ISNULL(@StartDate, '1/1/1970'))) AS AsOfDate
 	, Platform.Name AS PlatformName
 	, BudgetInfo.Date AS BudgetInfoDate
 	, PlatformBudgetInfo.Date AS PlatformBudgetInfoDate
@@ -15,6 +17,7 @@ SELECT ISNULL(PlatformBudgetInfo.MediaSpend, ISNULL(BudgetInfo.MediaSpend, Campa
 	, Campaign.AdvertiserId
 	, Account.Id AS AccountId
 	, Account.PlatformId
+	, CAST(Campaign.Id AS varchar) + ISNULL('-' + CONVERT(varchar, BudgetInfo.Date, 112), '') + ISNULL('-' + CAST(Account.Id AS varchar), '') + ISNULL('-' + CAST(Platform.Id AS varchar), '') + ISNULL('-' + CONVERT(varchar, PlatformBudgetInfo.Date, 112), '') AS UniqueId
 FROM td.Campaign
 LEFT OUTER JOIN td.BudgetInfo
    ON (BudgetInfo.CampaignId = Campaign.Id)
