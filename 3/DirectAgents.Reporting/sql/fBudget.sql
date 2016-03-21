81c786e5948efcf588b5959220bf00c77401b5a8
nathaniel-holder <nholder@DirectAgents.local>
2016-03-21 14:02:23 -0400

    Fixed Budget calculations

--- 3/DirectAgents.Reporting/sql/fBudget.sql
+++ 3/DirectAgents.Reporting/sql/fBudget.sql
@@ -9,29 +9,17 @@ BEGIN
 	(
 		SELECT ISNULL(BudgetInfo.MediaSpend, Campaign.MediaSpend) AS BudgetPart
 		, CASE WHEN ROW_NUMBER() OVER(PARTITION BY Campaign.Id ORDER BY BudgetInfo.Date DESC) = 1 THEN 1 END AS BudgetRow
-		, Account.Id AS AccountId
-		, Campaign.Id AS CampaignId
 		FROM td.Campaign
-		LEFT OUTER JOIN td.Account ON Account.CampaignId = Campaign.Id
 		LEFT OUTER JOIN td.BudgetInfo ON BudgetInfo.Id =
 			(
 			SELECT TOP 1 x.Id
 			FROM td.BudgetInfo x
-			WHERE (x.CampaignId = Account.CampaignId)
-				AND ((@StartDate IS NULL) OR (x.Date >= @StartDate))
-				AND ((@EndDate IS NULL) OR (x.Date <= @EndDate))
-			ORDER BY x.Date DESC
-			)
-		LEFT OUTER JOIN td.PlatformBudgetInfo ON PlatformBudgetInfo.Id =
-			(
-			SELECT TOP 1 x.Id
-			FROM td.PlatformBudgetInfo x
 			WHERE (x.CampaignId = Campaign.Id)
-				AND (x.PlatformId = Account.PlatformId)
 				AND ((@StartDate IS NULL) OR (x.Date >= @StartDate))
 				AND ((@EndDate IS NULL) OR (x.Date <= @EndDate))
 			ORDER BY x.Date DESC
 			)
+		LEFT OUTER JOIN td.Account ON Account.CampaignId = Campaign.Id
 		WHERE (td.Campaign.AdvertiserId = @AdvertiserId)
 	)
 	SELECT @ret = SUM(BudgetPart)
