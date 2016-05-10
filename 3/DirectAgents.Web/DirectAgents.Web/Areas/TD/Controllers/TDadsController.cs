4baae51ffd75f61521ec77cf9898ebedad709bc6
Nathaniel Holder <nholder@DirectAgents.local>
2016-05-10 11:27:42 -0400

    TDadsController

---
+++ 3/DirectAgents.Web/DirectAgents.Web/Areas/TD/Controllers/TDadsController.cs
@@ -0,0 +1,25 @@
+﻿using DirectAgents.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DirectAgents.Web.Areas.TD.Controllers
+{
+    public class TDadsController : DirectAgents.Web.Controllers.ControllerBase
+    {
+        public TDadsController(ITDRepository tdRepository)
+        {
+            this.tdRepo = tdRepository;
+        }
+
+        //
+        // GET: /TD/TDAd/
+        public ActionResult Index()
+        {
+            var ads = tdRepo.TDads(acctId: null).OrderBy(a => a.Name).ThenBy(a => a.Id);
+            return View(ads);
+        }
+	}
+}
