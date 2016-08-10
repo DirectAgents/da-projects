25e696f917a054aeae2af332761741d838dc6643
nathaniel-holder <apalmer@DirectAgents.local>
2016-08-10 12:10:44 -0400

    Added assign/unassign for advertisers/search

--- 3/ClientPortal.Web/ClientPortal.Web/Areas/Admin/Controllers/UsersController.cs
+++ 3/ClientPortal.Web/ClientPortal.Web/Areas/Admin/Controllers/UsersController.cs
@@ -78,5 +78,156 @@ namespace ClientPortal.Web.Areas.Admin.Controllers
             }
             return RedirectToAction("Setup", new { id = id });
         }
+
+        public ActionResult AssignProgAdvertiser(int id)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var userVM = new UserVM
+            {
+                UserProfile = userProfile,
+                CakeAdvertiser = userProfile.CakeAdvertiserId == null ? null : cpRepo.GetAdvertiser((int)userProfile.CakeAdvertiserId),
+                ProgAdvertiser = userProfile.TDAdvertiserId == null ? null : progRepo.Advertiser((int)userProfile.TDAdvertiserId)
+            };
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProgAdvertisers = progRepo.Advertisers().OrderBy(p => p.Name).ThenBy(p => p.Id);
+            return View(userVM);
+        }
+
+        [HttpPost]
+        public ActionResult AssignProgAdvertiser(int id, int progadvertiserid)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var userVM = new UserVM
+            {
+                UserProfile = userProfile,
+                CakeAdvertiser = userProfile.CakeAdvertiserId == null ? null : cpRepo.GetAdvertiser((int)userProfile.CakeAdvertiserId),
+                ProgAdvertiser = userProfile.TDAdvertiserId == null ? null : progRepo.Advertiser((int)userProfile.TDAdvertiserId)
+            };
+            var progAdvertiser = progRepo.Advertiser(progadvertiserid);
+
+            if (userProfile != null && progAdvertiser != null)
+            {
+                userVM.UserProfile.TDAdvertiserId = progadvertiserid;
+                userVM.ProgAdvertiser = progAdvertiser;
+                cpRepo.SaveChanges();
+                progRepo.SaveChanges();
+            }
+            return RedirectToAction("Setup", new { id = id });
+        }
+
+        public ActionResult UnassignProgAdvertiser(int id)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var userVM = new UserVM
+            {
+                UserProfile = userProfile,
+                CakeAdvertiser = userProfile.CakeAdvertiserId == null ? null : cpRepo.GetAdvertiser((int)userProfile.CakeAdvertiserId),
+                ProgAdvertiser = userProfile.TDAdvertiserId == null ? null : progRepo.Advertiser((int)userProfile.TDAdvertiserId)
+            };
+            if (userProfile != null && userVM.UserProfile.TDAdvertiserId.HasValue)
+            {
+                userVM.UserProfile.TDAdvertiserId = null;
+                userVM.ProgAdvertiser = null;
+                cpRepo.SaveChanges();
+                progRepo.SaveChanges();
+            }
+            return RedirectToAction("Setup", new { id = id });
+        }
+
+        public ActionResult AssignCakeAdvertiser(int id)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var userVM = new UserVM
+            {
+                UserProfile = userProfile,
+                CakeAdvertiser = userProfile.CakeAdvertiserId == null ? null : cpRepo.GetAdvertiser((int)userProfile.CakeAdvertiserId),
+                ProgAdvertiser = userProfile.TDAdvertiserId == null ? null : progRepo.Advertiser((int)userProfile.TDAdvertiserId)
+            };
+            if (userProfile == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CakeAdvertisers = cpRepo.Advertisers.OrderBy(c => c.AdvertiserName).ThenBy(c => c.AdvertiserId);
+            return View(userVM);
+        }
+
+        [HttpPost]
+        public ActionResult AssignCakeAdvertiser(int id, int cakeadvertiserid)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var userVM = new UserVM
+            {
+                UserProfile = userProfile,
+                CakeAdvertiser = userProfile.CakeAdvertiserId == null ? null : cpRepo.GetAdvertiser((int)userProfile.CakeAdvertiserId),
+                ProgAdvertiser = userProfile.TDAdvertiserId == null ? null : progRepo.Advertiser((int)userProfile.TDAdvertiserId)
+            };
+            var cakeAdvertiser = cpRepo.GetAdvertiser(cakeadvertiserid);
+
+            if (userProfile != null && cakeAdvertiser != null)
+            {
+                userVM.UserProfile.CakeAdvertiserId = cakeadvertiserid;
+                userVM.CakeAdvertiser = cakeAdvertiser;
+                cpRepo.SaveChanges();
+            }
+            return RedirectToAction("Setup", new { id = id });
+        }
+
+        public ActionResult UnassignCakeAdvertiser(int id)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var userVM = new UserVM
+            {
+                UserProfile = userProfile,
+                CakeAdvertiser = userProfile.CakeAdvertiserId == null ? null : cpRepo.GetAdvertiser((int)userProfile.CakeAdvertiserId),
+                ProgAdvertiser = userProfile.TDAdvertiserId == null ? null : progRepo.Advertiser((int)userProfile.TDAdvertiserId)
+            };
+            if (userProfile != null && userVM.UserProfile.CakeAdvertiserId.HasValue)
+            {
+                userVM.UserProfile.CakeAdvertiserId = null;
+                userVM.CakeAdvertiser = null;
+                cpRepo.SaveChanges();
+            }
+            return RedirectToAction("Setup", new { id = id });
+        }
+
+        public ActionResult AssignSearchProfile(int id)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            if (userProfile == null)
+                return HttpNotFound();
+
+            ViewBag.SearchProfiles = cpRepo.SearchProfiles().OrderBy(s => s.SearchProfileName).ThenBy(s => s.SearchProfileId);
+            return View(userProfile);
+        }
+
+        [HttpPost]
+        public ActionResult AssignSearchProfile(int id, int searchprofileid)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            var searchProfile = cpRepo.GetSearchProfile(searchprofileid);
+            if (userProfile != null && searchProfile != null)
+            {
+                userProfile.SearchProfile = searchProfile;
+                userProfile.SearchProfileId = searchProfile.SearchProfileId;
+                
+                cpRepo.SaveChanges();
+            }
+            return RedirectToAction("Setup", new { id = id });
+        }
+
+        public ActionResult UnassignSearchProfile(int id)
+        {
+            var userProfile = cpRepo.GetUserProfile(id);
+            if (userProfile != null && userProfile.SearchProfileId.HasValue)
+            {
+                userProfile.SearchProfileId = null;
+                userProfile.SearchProfile = null;
+                cpRepo.SaveChanges();
+            }
+            return RedirectToAction("Setup", new { id = id });
+        }
     }
 }
