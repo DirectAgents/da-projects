c761a5134abc2513e6ac502575ac41a7674dc889
Kevin Slesinsky <kevin@directagents.com>
2016-10-13 10:51:12 -0400

    Rework "Daily Stats" kendo spreadsheet

--- 3/DirectAgents.Web/DirectAgents.Web/Areas/TD/Models/ReportingVM.cs
+++ 3/DirectAgents.Web/DirectAgents.Web/Areas/TD/Models/ReportingVM.cs
@@ -11,5 +11,44 @@ namespace DirectAgents.Web.Areas.TD.Models
         public Campaign Campaign { get; set; }
         public DateTime? Start { get; set; }
         public DateTime? End { get; set; }
+
+        public IEnumerable<ColumnConfig> ColumnConfigs { get; set; }
+    }
+
+    public class ColumnConfig
+    {
+        public const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string Format_Date = "dd/mm/yyyy";
+        public const string Format_Integer = "#,0";
+        public const string Format_DollarCents = "$#,0.00";
+        public const string Format_Percent2Dec = "#,0.00%";
+
+        public string PropName { get; set; }
+        public string DisplayName { get; set; }
+        public string Letter { get; set; }
+        public string Format { get; set; }
+
+        public ColumnConfig(string propName, string displayName, char letter, string format)
+        {
+            this.PropName = propName;
+            this.DisplayName = displayName;
+            this.Letter = new String(letter, 1);
+            this.Format = format;
+        }
+
+        public static IEnumerable<ColumnConfig> BasicColumns() {
+            var cols = new List<ColumnConfig>();
+            int letter = 1;
+            cols.Add(new ColumnConfig("Date", null, Alphabet[letter++], ColumnConfig.Format_Date));
+            cols.Add(new ColumnConfig("Impressions", null, Alphabet[letter++], ColumnConfig.Format_Integer));
+            cols.Add(new ColumnConfig("Clicks", null, Alphabet[letter++], ColumnConfig.Format_Integer));
+            cols.Add(new ColumnConfig("TotalConv", null, Alphabet[letter++], ColumnConfig.Format_Integer));
+            cols.Add(new ColumnConfig("PostClickConv", null, Alphabet[letter++], ColumnConfig.Format_Integer));
+            cols.Add(new ColumnConfig("PostViewConv", null, Alphabet[letter++], ColumnConfig.Format_Integer));
+            cols.Add(new ColumnConfig("Spend", null, Alphabet[letter++], ColumnConfig.Format_DollarCents));
+            cols.Add(new ColumnConfig("CTR", null, Alphabet[letter++], ColumnConfig.Format_Percent2Dec));
+            cols.Add(new ColumnConfig("CPA", null, Alphabet[letter++], ColumnConfig.Format_DollarCents));
+            return cols;
+        }
     }
 }
