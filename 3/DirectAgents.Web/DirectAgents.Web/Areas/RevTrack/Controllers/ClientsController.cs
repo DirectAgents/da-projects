cd180920defcda473026355a66083cb93ce55b08
Kevin Slesinsky <kevin@directagents.com>
2016-11-11 16:48:55 -0500

    DAWeb- RevTrack area

---
+++ 3/DirectAgents.Web/DirectAgents.Web/Areas/RevTrack/Controllers/ClientsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DirectAgents.Web.Areas.RevTrack.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("test");
+        }
+	}
+}
