e74e91756dab05ebc923205036c298d28d162c46
Kevin Slesinsky <kevin@directagents.com>
2016-11-14 17:13:48 -0500

    VendorProg, move "Code" to Prog entities

--- 1/DirectAgents.Domain/Entities/RevTrack/ClientVendor.cs
+++ 1/DirectAgents.Domain/Entities/RevTrack/ClientVendor.cs
@@ -9,17 +9,16 @@ namespace DirectAgents.Domain.Entities.RevTrack
     public class Client
     {
         public int Id { get; set; }
-        [MaxLength(50), Index("CodeIndex", IsUnique = true)]
-        public string Code { get; set; }
         public string Name { get; set; }
 
         public virtual ClientProg Prog { get; set; }
     }
-
     public class ClientProg //"ProgInfo" for a client
     {
         [Key, ForeignKey("Client")]
         public int ClientId { get; set; }
+        [MaxLength(50), Index("CodeIndex", IsUnique = true)]
+        public string Code { get; set; }
 
         public BudgetInfoVals DefaultBudgetInfo { get; set; }
 
@@ -27,6 +26,8 @@ namespace DirectAgents.Domain.Entities.RevTrack
         public virtual ICollection<ProgBudgetInfo> BudgetInfos { get; set; }
         public virtual ICollection<ProgVendorBudgetInfo> VendorBudgetInfos { get; set; }
     }
+
+    // --- BudgetInfos ---
 
     public class ProgBudgetInfo : BudgetInfoVals
     {
@@ -45,12 +46,21 @@ namespace DirectAgents.Domain.Entities.RevTrack
         public virtual Vendor Vendor { get; set; }
     }
 
+    // --- Vendor ---
+
     public class Vendor
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class VendorProg //"ProgInfo" for a vendor
+    {
+        [Key, ForeignKey("Vendor")]
+        public int VendorId { get; set; }
         [MaxLength(50), Index("CodeIndex", IsUnique = true)]
         public string Code { get; set; }
-        public string Name { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
     }
 
 }
--- 1/DirectAgents.Domain/MigrationsRT/201611112036125_InitialCreate.cs
+++ 1/DirectAgents.Domain/MigrationsRT/201611112036125_InitialCreate.cs
@@ -12,13 +12,15 @@ namespace DirectAgents.Domain.MigrationsRT
                 c => new
                     {
                         ClientId = c.Int(nullable: false),
+                        Code = c.String(maxLength: 50),
                         MediaSpend = c.Decimal(nullable: false, precision: 14, scale: 2),
                         MgmtFeePct = c.Decimal(nullable: false, precision: 10, scale: 5),
                         MarginPct = c.Decimal(nullable: false, precision: 10, scale: 5),
                     })
                 .PrimaryKey(t => t.ClientId)
                 .ForeignKey("ad.Client", t => t.ClientId)
-                .Index(t => t.ClientId);
+                .Index(t => t.ClientId)
+                .Index(t => t.Code, unique: true, name: "CodeIndex");
             
             CreateTable(
                 "ad.ProgBudgetInfo",
@@ -40,11 +42,9 @@ namespace DirectAgents.Domain.MigrationsRT
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Code = c.String(maxLength: 50),
                         Name = c.String(),
                     })
-                .PrimaryKey(t => t.Id)
-                .Index(t => t.Code, unique: true, name: "CodeIndex");
+                .PrimaryKey(t => t.Id);
             
             CreateTable(
                 "ad.ProgVendorBudgetInfo",
@@ -69,28 +69,41 @@ namespace DirectAgents.Domain.MigrationsRT
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Code = c.String(maxLength: 50),
                         Name = c.String(),
                     })
-                .PrimaryKey(t => t.Id)
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "ad.VendorProg",
+                c => new
+                    {
+                        VendorId = c.Int(nullable: false),
+                        Code = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.VendorId)
+                .ForeignKey("ad.Vendor", t => t.VendorId)
+                .Index(t => t.VendorId)
                 .Index(t => t.Code, unique: true, name: "CodeIndex");
             
         }
         
         public override void Down()
         {
+            DropForeignKey("ad.VendorProg", "VendorId", "ad.Vendor");
             DropForeignKey("ad.ProgVendorBudgetInfo", "ClientId", "ad.ClientProg");
             DropForeignKey("ad.ProgVendorBudgetInfo", "VendorId", "ad.Vendor");
             DropForeignKey("ad.ProgVendorBudgetInfo", "ClientId", "ad.Client");
             DropForeignKey("ad.ClientProg", "ClientId", "ad.Client");
             DropForeignKey("ad.ProgBudgetInfo", "ClientId", "ad.ClientProg");
             DropForeignKey("ad.ProgBudgetInfo", "ClientId", "ad.Client");
-            DropIndex("ad.Vendor", "CodeIndex");
+            DropIndex("ad.VendorProg", "CodeIndex");
+            DropIndex("ad.VendorProg", new[] { "VendorId" });
             DropIndex("ad.ProgVendorBudgetInfo", new[] { "VendorId" });
             DropIndex("ad.ProgVendorBudgetInfo", new[] { "ClientId" });
-            DropIndex("ad.Client", "CodeIndex");
             DropIndex("ad.ProgBudgetInfo", new[] { "ClientId" });
+            DropIndex("ad.ClientProg", "CodeIndex");
             DropIndex("ad.ClientProg", new[] { "ClientId" });
+            DropTable("ad.VendorProg");
             DropTable("ad.Vendor");
             DropTable("ad.ProgVendorBudgetInfo");
             DropTable("ad.Client");
