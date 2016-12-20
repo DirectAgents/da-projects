edef18e0e0a52df75b447b4cf7f40b21f63e827c
Kevin Slesinsky <kevin@directagents.com>
2016-12-20 14:09:23 -0500

    ABContext

---
+++ 1/DirectAgents.Domain/Contexts/ABContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using DirectAgents.Domain.Entities.AB;
+
+namespace DirectAgents.Domain.Contexts
+{
+    public class ABContext : DbContext
+    {
+        public const string abSchema = "ab";
+
+        //? set CommandTimeout in constructor ?
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            // --- AB ---
+            modelBuilder.Entity<ABClient>().ToTable("Client", abSchema);
+        }
+
+        // --- AB ---
+        public DbSet<ABClient> ABClients { get; set; }
+    }
+}
