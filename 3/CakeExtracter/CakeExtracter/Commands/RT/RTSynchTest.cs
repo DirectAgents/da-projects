a86315f73d76c1c161fcd8cda42c4e3044190ae4
Kevin Slesinsky <kevin@directagents.com>
2017-01-05 20:01:53 -0500

    updating rt synchers

--- 3/CakeExtracter/CakeExtracter/Commands/RT/RTSynchTest.cs
+++ 3/CakeExtracter/CakeExtracter/Commands/RT/RTSynchTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using CakeExtracter.Common;
@@ -21,37 +22,64 @@ namespace CakeExtracter.Commands.RT
 
         public override int Execute(string[] remainingArguments)
         {
-            CopySummaries(clientId: 16);
+            CopyBudgetInfos();
+            //CopySummaries(clientId: 16);
             return 0;
         }
 
+        //TODO
+        // a way to compare the two sides (for each entity)... deletions, changes, additions
+        // then, make the copy methods check what's already there, and only copy the new entities
+        // ?what to do about changes and deletions?
+        // a generic compare screen (view) - id & name of entity?
+
+        //TODO: ?deletions - how to handle?
+        //      ABClientId ?
         public void CopyProgClients()
         {
             using (var cpprogContext = new ClientPortalProgContext())
             using (var rtContext = new RevTrackContext())
             {
+                //var existingPCids = rtContext.ProgClients.Select(x => x.Id).ToArray();
+
                 var cppAdvertisers = cpprogContext.Advertisers;
-                foreach (var cppAdv in cppAdvertisers)
+                int numChanged = 0;
+                foreach (var cppAdv in cppAdvertisers.OrderBy(x => x.Id))
                 {
-                    var progClient = new ProgClient
+                    //if (!existingPCids.Contains(cppAdv.Id)) // add
+                    var existing = rtContext.ProgClients.Find(cppAdv.Id);
+                    if (existing == null) // add
                     {
-                        Id = cppAdv.Id,
-                        Name = cppAdv.Name
-                    };
-                    rtContext.ProgClients.Add(progClient);
+                        var progClient = new ProgClient
+                        {
+                            Id = cppAdv.Id,
+                            Name = cppAdv.Name
+                        };
+                        rtContext.ProgClients.Add(progClient);
+                    }
+                    else // update
+                    {
+                        if (existing.Name != cppAdv.Name)
+                        {
+                            existing.Name = cppAdv.Name;
+                            numChanged++;
+                        }
+                    }
                 }
                 rtContext.SaveChanges();
             }
         }
+        //TODO: ?deletions - how to handle?
         public void CopyCampaigns()
         {
             using (var cpprogContext = new ClientPortalProgContext())
             using (var rtContext = new RevTrackContext())
             {
                 var cppCampaigns = cpprogContext.Campaigns;
-                foreach (var cppCamp in cppCampaigns)
+                int numChanged = 0;
+                foreach (var cppCamp in cppCampaigns.OrderBy(x => x.Id))
                 {
-                    var progCampaign = new ProgCampaign
+                    var fresh = new ProgCampaign
                     {
                         Id = cppCamp.Id,
                         ProgClientId = cppCamp.AdvertiserId,
@@ -63,31 +91,87 @@ namespace CakeExtracter.Commands.RT
                             MarginPct = cppCamp.DefaultBudgetInfo.MarginPct
                         }
                     };
-                    rtContext.ProgCampaigns.Add(progCampaign);
+                    var existing = rtContext.ProgCampaigns.Find(fresh.Id);
+                    if (existing == null) // add
+                    {
+                        rtContext.ProgCampaigns.Add(fresh);
+                    }
+                    else // update
+                    {
+                        if (!PropertyCompare.Equal(existing, fresh) || !PropertyCompare.Equal(existing.DefaultBudgetInfo, fresh.DefaultBudgetInfo))
+                        {
+                            existing.ProgClientId = fresh.ProgClientId;
+                            existing.Name = fresh.Name;
+                            existing.DefaultBudgetInfo.SetFrom(fresh.DefaultBudgetInfo);
+                            numChanged++;
+                        }
+                    }
                 }
                 rtContext.SaveChanges();
             }
         }
+
         public void CopyBudgetInfos()
         {
             using (var cpprogContext = new ClientPortalProgContext())
             using (var rtContext = new RevTrackContext())
             {
-                var cppBudgetInfos = cpprogContext.BudgetInfos;
-                foreach (var cppBI in cppBudgetInfos)
+                var cppBudgetInfos = cpprogContext.BudgetInfos; // fresh BIs
+                var rtBudgetInfos = rtContext.ProgBudgetInfos; // existing BIs
+
+                var freshCampIds = cppBudgetInfos.Select(x => x.CampaignId).Distinct().ToArray();
+                var rtBIsToDelete = rtBudgetInfos.Where(x => !freshCampIds.Contains(x.ProgCampaignId));
+                foreach (var rtBItoDelete in rtBIsToDelete)
                 {
-                    var progBudgetInfo = new ProgBudgetInfo
-                    {
-                        ProgCampaignId = cppBI.CampaignId,
-                        Date = cppBI.Date,
-                        MediaSpend = cppBI.MediaSpend,
-                        MgmtFeePct = cppBI.MgmtFeePct,
-                        MarginPct = cppBI.MarginPct
-                    };
-                    rtContext.ProgBudgetInfos.Add(progBudgetInfo);
+                    rtContext.ProgBudgetInfos.Remove(rtBItoDelete);
                 }
+
+                // Group "fresh" BIs by campaign
+                var cppCampGroups = cppBudgetInfos.GroupBy(x => x.CampaignId);
+                foreach (var cppCampGroup in cppCampGroups)
+                {
+                    var rtCampBIs = rtBudgetInfos.Where(x => x.ProgCampaignId == cppCampGroup.Key);
+                    var updatedBIDates = new List<DateTime>();
+                    foreach (var rtBI in rtCampBIs.OrderBy(x => x.Date))
+                    {
+                        var cppBI = cppCampGroup.FirstOrDefault(x => x.Date == rtBI.Date);
+                        if (cppBI == null) // delete
+                            rtContext.ProgBudgetInfos.Remove(rtBI);
+                        else // update
+                        {
+                            rtBI.SetFrom(cppBI);
+                            updatedBIDates.Add(rtBI.Date);
+                        }
+                    }
+                    // Handle additions
+                    var newBIs = cppCampGroup.Where(x => !updatedBIDates.Contains(x.Date));
+                    foreach (var newBI in newBIs)
+                    {
+                        var progBudgetInfo = new ProgBudgetInfo
+                        {
+                            ProgCampaignId = newBI.CampaignId,
+                            Date = newBI.Date,
+                            MediaSpend = newBI.MediaSpend,
+                            MgmtFeePct = newBI.MgmtFeePct,
+                            MarginPct = newBI.MarginPct
+                        };
+                        rtContext.ProgBudgetInfos.Add(progBudgetInfo);
+                    }
+                }
+                int numWrites = rtContext.SaveChanges();
+            }
+        }
+
+        //TODO: new strategy; load fresh PBIs into memory. loop through and put "additions" in a new List. (add to db later)
+        // then, loop through existings. delete or update. then do additions from list.
+
+        public void CopyPlatformBudgetInfos()
+        {
+            using (var cpprogContext = new ClientPortalProgContext())
+            using (var rtContext = new RevTrackContext())
+            {
                 var cppPBIs = cpprogContext.PlatformBudgetInfos;
-                foreach (var cppPBI in cppPBIs)
+                foreach (var cppPBI in cppPBIs.OrderBy(x => x.CampaignId).ThenBy(x => x.Date).ThenBy(x => x.PlatformId))
                 {
                     var progVendorBudgetInfo = new ProgVendorBudgetInfo
                     {
@@ -110,7 +194,7 @@ namespace CakeExtracter.Commands.RT
             using (var rtContext = new RevTrackContext())
             {
                 var platforms = cpprogContext.Platforms;
-                foreach (var platform in platforms)
+                foreach (var platform in platforms.OrderBy(x => x.Id))
                 {
                     var progVendor = new ProgVendor
                     {
@@ -133,11 +217,11 @@ namespace CakeExtracter.Commands.RT
                 if (progClient != null)
                 {
                     var campIds = progClient.ProgCampaigns.Select(c => c.Id).ToList();
-                    foreach (var campId in campIds)
+                    foreach (var campId in campIds.OrderBy(x => x))
                     {
                         var extAccounts = cpprogContext.ExtAccounts.Where(a => a.CampaignId.HasValue && a.CampaignId.Value == campId);
                         var platformGroups = extAccounts.GroupBy(a => a.PlatformId);
-                        foreach (var platformGroup in platformGroups)
+                        foreach (var platformGroup in platformGroups.OrderBy(x => x.Key))
                         {
                             var acctIds = platformGroup.Select(g => g.Id).ToList();
                             var dSums = cpprogContext.DailySummaries.Where(ds => acctIds.Contains(ds.AccountId));
@@ -145,7 +229,9 @@ namespace CakeExtracter.Commands.RT
                             {
                                 Year = ds.Date.Year,
                                 Month = ds.Date.Month
-                            }).AsEnumerable()
+                            }).OrderBy(g => g.Key.Year)
+                              .ThenBy(g => g.Key.Month)
+                              .AsEnumerable()
                               .Select(g => new ProgSummary
                               {
                                   Date = new DateTime(g.Key.Year, g.Key.Month, 1),
