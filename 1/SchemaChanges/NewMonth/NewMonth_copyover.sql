6794ee19a6dc9bd7c9ec503aaba10dfd2639e107
Kevin Slesinsky <kevin@directagents.com>
2017-01-19 10:22:15 -0500

    misc- configs,etc

--- 1/SchemaChanges/NewMonth/NewMonth_copyover.sql
+++ 1/SchemaChanges/NewMonth/NewMonth_copyover.sql
@@ -1,11 +1,11 @@
 -- *TODO: SET APPROPRIATE DATABASES!*
-USE DADatabaseB...2016
+USE DADatabaseB...2017
 
 --Disable triggers
 DISABLE TRIGGER tr_Affiliate_IU ON Affiliate
 
 --Copy from previous month; *SET DATABASES!*
-exec DAMain1.dbo.EOMcopy 'DADatabaseA...2016','DADatabaseB...2016'
+exec DAMain1.dbo.EOMcopy 'DADatabaseA...2017','DADatabaseB...2017'
 
 --Clear affiliate dates
 UPDATE Affiliate SET date_created=NULL, date_modified=NULL
@@ -24,7 +24,7 @@ ENABLE TRIGGER tr_Affiliate_IU ON Affiliate
 --Initialize audit (changes current db)
 :r "C:\GitHub\da-projects-kevin\1\SchemaChanges\AuditScripts\autoaudit 2.00h.sql"
 
-USE DADatabaseB...2016
+USE DADatabaseB...2017
 
 --Add audits
 :r "C:\GitHub\da-projects-kevin\1\SchemaChanges\AuditScripts\AddAuditTables.sql"
