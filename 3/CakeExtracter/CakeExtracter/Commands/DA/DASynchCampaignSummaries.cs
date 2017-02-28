93553b07d3878d7cce04aca037a9efb70bbc8073
Kevin Slesinsky <kevin@directagents.com>
2017-02-28 17:43:35 -0500

    oops- Camp ETL code not there yet

--- 3/CakeExtracter/CakeExtracter/Commands/DA/DASynchCampaignSummaries.cs
+++ 3/CakeExtracter/CakeExtracter/Commands/DA/DASynchCampaignSummaries.cs
@@ -77,23 +77,23 @@ namespace CakeExtracter.Commands
 
         private void LoadMissingCampaigns(IEnumerable<int> campIdsToCheck)
         {
-            int[] existingCampIds;
-            using (var db = new DAContext())
-            {
-                existingCampIds = db.Camps.Select(x => x.CampaignId).ToArray();
-            }
-            var campIdsToLoad = new List<int>();
-            foreach (var campIdToCheck in campIdsToCheck)
-            {
-                if (!existingCampIds.Contains(campIdToCheck))
-                    campIdsToLoad.Add(campIdToCheck);
-            }
-            var extracter = new CampaignsExtracter(campaignIds: campIdsToLoad);
-            var loader = new DACampLoader();
-            var extracterThread = extracter.Start();
-            var loaderThread = loader.Start(extracter);
-            extracterThread.Join();
-            loaderThread.Join();
+            //int[] existingCampIds;
+            //using (var db = new DAContext())
+            //{
+            //    existingCampIds = db.Camps.Select(x => x.CampaignId).ToArray();
+            //}
+            //var campIdsToLoad = new List<int>();
+            //foreach (var campIdToCheck in campIdsToCheck)
+            //{
+            //    if (!existingCampIds.Contains(campIdToCheck))
+            //        campIdsToLoad.Add(campIdToCheck);
+            //}
+            //var extracter = new CampaignsExtracter(campaignIds: campIdsToLoad);
+            //var loader = new DACampLoader();
+            //var extracterThread = extracter.Start();
+            //var loaderThread = loader.Start(extracter);
+            //extracterThread.Join();
+            //loaderThread.Join();
         }
     }
 }
