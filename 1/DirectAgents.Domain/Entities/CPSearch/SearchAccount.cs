fd7aba36777c954fa23b92c12c7b40cab445cc5e
Kevin Slesinsky <kevin@directagents.com>
2018-01-19 15:10:27 -0500

    CPSearch entities

---
+++ 1/DirectAgents.Domain/Entities/CPSearch/SearchAccount.cs
@@ -0,0 +1,50 @@
+﻿namespace DirectAgents.Domain.Entities.CPSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("SearchAccount")]
+    public partial class SearchAccount
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SearchAccount()
+        {
+            SearchCampaigns = new HashSet<SearchCampaign>();
+            AltSearchCampaigns = new HashSet<SearchCampaign>();
+        }
+
+        public int SearchAccountId { get; set; }
+
+        public int? AdvertiserId { get; set; }
+
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Channel { get; set; }
+
+        [StringLength(50)]
+        public string AccountCode { get; set; }
+
+        [StringLength(50)]
+        public string ExternalId { get; set; }
+
+        public int? SearchProfileId { get; set; }
+
+        public decimal? RevPerOrder { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime? MinSynchDate { get; set; }
+
+        public virtual SearchProfile SearchProfile { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SearchCampaign> SearchCampaigns { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SearchCampaign> AltSearchCampaigns { get; set; }
+    }
+}
---
+++ 1/DirectAgents.Domain/Entities/CPSearch/SearchCampaign.cs
@@ -0,0 +1,49 @@
+﻿namespace DirectAgents.Domain.Entities.CPSearch
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("SearchCampaign")]
+    public partial class SearchCampaign
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SearchCampaign()
+        {
+            SearchConvSummaries = new HashSet<SearchConvSummary>();
+            SearchDailySummaries = new HashSet<SearchDailySummary>();
+        }
+
+        public int SearchCampaignId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string SearchCampaignName { get; set; }
+
+        public int? AdvertiserId { get; set; }
+
+        [StringLength(255)]
+        public string Channel { get; set; }
+
+        public int? ExternalId { get; set; }
+
+        public int? SearchAccountId { get; set; }
+
+        public int? AltSearchAccountId { get; set; }
+
+        [StringLength(100)]
+        public string LCcmpid { get; set; }
+
+        public virtual SearchAccount SearchAccount { get; set; }
+
+        public virtual SearchAccount AltSearchAccount { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SearchConvSummary> SearchConvSummaries { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SearchDailySummary> SearchDailySummaries { get; set; }
+    }
+}
