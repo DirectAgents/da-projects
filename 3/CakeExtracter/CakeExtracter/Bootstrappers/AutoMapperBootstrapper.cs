4bacfa89e7ccff4371c9cd8710ed5d90f79b2bec
Kevin Slesinsky <kevin@directagents.com>
2018-07-02 10:38:14 -0400

    upgrade AutoMapper

--- 3/CakeExtracter/CakeExtracter/Bootstrappers/AutoMapperBootstrapper.cs
+++ 3/CakeExtracter/CakeExtracter/Bootstrappers/AutoMapperBootstrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.Composition;
+﻿using System;
+using System.ComponentModel.Composition;
 using AutoMapper;
 using ClientPortal.Data.Contexts;
 
@@ -14,43 +15,51 @@ namespace CakeExtracter.Bootstrappers
 
         public static void CreateMaps()
         {
-            Mapper.CreateMap<SearchDailySummary, SearchDailySummary>();
-            Mapper.CreateMap<SearchDailySummary2, SearchDailySummary2>();
-            //Mapper.CreateMap<OfferDailySummary, OfferDailySummary>();
-            Mapper.CreateMap<GoogleAnalyticsSummary, GoogleAnalyticsSummary>();
-            Mapper.CreateMap<CallDailySummary, CallDailySummary>();
-            Mapper.CreateMap<SearchConvSummary, SearchConvSummary>();
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<SearchDailySummary, SearchDailySummary>();
+                cfg.CreateMap<SearchDailySummary2, SearchDailySummary2>();
+                //cfg.CreateMap<OfferDailySummary, OfferDailySummary>();
+                cfg.CreateMap<GoogleAnalyticsSummary, GoogleAnalyticsSummary>();
+                cfg.CreateMap<CallDailySummary, CallDailySummary>();
+                cfg.CreateMap<SearchConvSummary, SearchConvSummary>();
 
-            Mapper.CreateMap<ClientPortal.Data.Entities.TD.DBM.CreativeDailySummary, ClientPortal.Data.Entities.TD.DBM.CreativeDailySummary>();
-            Mapper.CreateMap<ClientPortal.Data.Entities.TD.DBM.DBMDailySummary, ClientPortal.Data.Entities.TD.DBM.DBMDailySummary>();
-            Mapper.CreateMap<ClientPortal.Data.Entities.TD.DBM.DBMConversion, ClientPortal.Data.Entities.TD.DBM.DBMConversion>();
-            Mapper.CreateMap<ClientPortal.Data.Entities.TD.DBM.UserListStat, ClientPortal.Data.Entities.TD.DBM.UserListStat>();
-            Mapper.CreateMap<ClientPortal.Data.Entities.TD.AdRoll.AdDailySummary, ClientPortal.Data.Entities.TD.AdRoll.AdDailySummary>();
+                cfg.CreateMap<ClientPortal.Data.Entities.TD.DBM.CreativeDailySummary, ClientPortal.Data.Entities.TD.DBM.CreativeDailySummary>();
+                cfg.CreateMap<ClientPortal.Data.Entities.TD.DBM.DBMDailySummary, ClientPortal.Data.Entities.TD.DBM.DBMDailySummary>();
+                cfg.CreateMap<ClientPortal.Data.Entities.TD.DBM.DBMConversion, ClientPortal.Data.Entities.TD.DBM.DBMConversion>();
+                cfg.CreateMap<ClientPortal.Data.Entities.TD.DBM.UserListStat, ClientPortal.Data.Entities.TD.DBM.UserListStat>();
+                cfg.CreateMap<ClientPortal.Data.Entities.TD.AdRoll.AdDailySummary, ClientPortal.Data.Entities.TD.AdRoll.AdDailySummary>();
 
-            Mapper.CreateMap<DirectAgents.Domain.Entities.CPProg.DailySummary, DirectAgents.Domain.Entities.CPProg.DailySummary>();
-            Mapper.CreateMap<DirectAgents.Domain.Entities.CPProg.StrategySummary, DirectAgents.Domain.Entities.CPProg.StrategySummary>()
-                .ForMember(s => s.StrategyName, opt => opt.Ignore())
-                .ForMember(s => s.StrategyEid, opt => opt.Ignore());
-            Mapper.CreateMap<DirectAgents.Domain.Entities.CPProg.AdSetSummary, DirectAgents.Domain.Entities.CPProg.AdSetSummary>()
-                .ForMember(s => s.AdSetName, opt => opt.Ignore())
-                .ForMember(s => s.AdSetEid, opt => opt.Ignore())
-                .ForMember(s => s.StrategyName, opt => opt.Ignore())
-                .ForMember(s => s.StrategyEid, opt => opt.Ignore());
-            Mapper.CreateMap<DirectAgents.Domain.Entities.CPProg.TDadSummary, DirectAgents.Domain.Entities.CPProg.TDadSummary>()
-                .ForMember(s => s.TDadName, opt => opt.Ignore())
-                .ForMember(s => s.TDadEid, opt => opt.Ignore());
-            Mapper.CreateMap<DirectAgents.Domain.Entities.CPProg.SiteSummary, DirectAgents.Domain.Entities.CPProg.SiteSummary>()
-                .ForMember(s => s.SiteName, opt => opt.Ignore());
-            Mapper.CreateMap<DirectAgents.Domain.Entities.CPProg.Conv, DirectAgents.Domain.Entities.CPProg.Conv>();
-            Mapper.CreateMap<DirectAgents.Domain.Entities.AdRoll.AdDailySummary, DirectAgents.Domain.Entities.AdRoll.AdDailySummary>();
-            Mapper.CreateMap<DirectAgents.Domain.Entities.DBM.CreativeDailySummary, DirectAgents.Domain.Entities.DBM.CreativeDailySummary>();
+                cfg.CreateMap<DirectAgents.Domain.Entities.CPProg.DailySummary, DirectAgents.Domain.Entities.CPProg.DailySummary>();
+                cfg.CreateMap<DirectAgents.Domain.Entities.CPProg.StrategySummary, DirectAgents.Domain.Entities.CPProg.StrategySummary>()
+                    .ForMember(s => s.StrategyName, opt => opt.Ignore())
+                    .ForMember(s => s.StrategyEid, opt => opt.Ignore());
+                cfg.CreateMap<DirectAgents.Domain.Entities.CPProg.AdSetSummary, DirectAgents.Domain.Entities.CPProg.AdSetSummary>()
+                    .ForMember(s => s.AdSetName, opt => opt.Ignore())
+                    .ForMember(s => s.AdSetEid, opt => opt.Ignore())
+                    .ForMember(s => s.StrategyName, opt => opt.Ignore())
+                    .ForMember(s => s.StrategyEid, opt => opt.Ignore());
+                cfg.CreateMap<DirectAgents.Domain.Entities.CPProg.TDadSummary, DirectAgents.Domain.Entities.CPProg.TDadSummary>()
+                    .ForMember(s => s.TDadName, opt => opt.Ignore())
+                    .ForMember(s => s.TDadEid, opt => opt.Ignore());
+                cfg.CreateMap<DirectAgents.Domain.Entities.CPProg.SiteSummary, DirectAgents.Domain.Entities.CPProg.SiteSummary>()
+                    .ForMember(s => s.SiteName, opt => opt.Ignore());
+                cfg.CreateMap<DirectAgents.Domain.Entities.CPProg.Conv, DirectAgents.Domain.Entities.CPProg.Conv>();
+                cfg.CreateMap<DirectAgents.Domain.Entities.AdRoll.AdDailySummary, DirectAgents.Domain.Entities.AdRoll.AdDailySummary>();
+                cfg.CreateMap<DirectAgents.Domain.Entities.DBM.CreativeDailySummary, DirectAgents.Domain.Entities.DBM.CreativeDailySummary>();
+            });
         }
 
         // Called from the commands' RunStatic() methods
         // TODO? A static method that checks all bootstrappers. e.g. ServicePointBootstrapper ... DefaultConnectionLimit
         public static void CheckRunSetup()
         {
-            if (Mapper.GetAllTypeMaps().Length == 0)
+            try
+            {
+                Mapper.Configuration.AssertConfigurationIsValid();
+            }
+            //catch (AutoMapperConfigurationException) // didn't get caught
+            catch (Exception)
             {
                 CreateMaps();
             }
