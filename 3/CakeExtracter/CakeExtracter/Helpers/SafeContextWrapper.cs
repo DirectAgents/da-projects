8059459212225016ddf3341f9a453fd01a1bd58a
Kevin Slesinsky <kevin@directagents.com>
2018-12-10 15:31:36 -0500

    restore code lost in merge

--- 3/CakeExtracter/CakeExtracter/Helpers/SafeContextWrapper.cs
+++ 3/CakeExtracter/CakeExtracter/Helpers/SafeContextWrapper.cs
@@ -17,6 +17,9 @@ namespace CakeExtracter.Helpers
         public static object KeywordLocker = new object();
         public static object SearchTermLocker = new object();
 
+        private const int RetryCount = 5;
+        private const int WaitSeconds = 3;
+
         public static int SaveChangedContext<T>(object contextLocker, Action<T> changeContextAction)
             where T : DbContext, new()
         {
@@ -49,6 +52,149 @@ namespace CakeExtracter.Helpers
             {
                 changeContextAction();
             }
+        }
+
+        public static int TrySaveChanges<T>(T dbContext, object contextLocker, Action<T> changeContextAction)
+            where T : DbContext, new()
+        {
+            var numChanges = 0;
+            try
+            {
+                numChanges = SaveChanges(dbContext);
+            }
+            catch (Exception exception)
+            {
+                if (exception is DbUpdateException)
+                {
+                    LogEndOfRetrying(exception);
+                    return numChanges;
+                }
+                numChanges = TrySaveChanges(contextLocker, changeContextAction);
+            }
+
+            return numChanges;
+        }
+
+        public static int TrySaveChanges<T>(T dbContext)
+            where T : DbContext, new()
+        {
+            try
+            {
+                return SaveChanges(dbContext);
+            }
+            catch (Exception exception)
+            {
+                LogEndOfRetrying(exception);
+            }
+
+            return 0;
+        }
+
+        private static int TrySaveChanges<T>(object contextLocker, Action<T> changeContextAction)
+            where T : DbContext, new()
+        {
+            var numChanges = 0;
+            try
+            {
+                Logger.Warn("Trying to save changes using reopened database context.");
+                numChanges = SaveChanges(contextLocker, changeContextAction);
+            }
+            catch (Exception exc)
+            {
+                LogEndOfRetrying(exc);
+            }
+
+            return numChanges;
+        }
+
+        private static int SaveChanges<T>(T dbContext)
+            where T : DbContext
+        {
+            var numChanges = Policy
+                .Handle<Exception>()
+                .WaitAndRetry(RetryCount,
+                    retryNumber => new TimeSpan(0, 0, WaitSeconds),
+                    (exc, time, context) => ProcessSaveException(exc))
+                .Execute(dbContext.SaveChanges);
+            return numChanges;
+        }
+
+        private static int SaveChanges<T>(object contextLocker, Action<T> changeContextAction)
+            where T : DbContext, new()
+        {
+            var numChanges = Policy
+                .Handle<Exception>()
+                .Retry(RetryCount, (exc, retryCount) => ProcessSaveException(exc))
+                .Execute(() => SaveChangedContextWithoutRetrying(contextLocker, changeContextAction));
+            return numChanges;
+        }
+
+        private static int SaveChangedContextWithoutRetrying<T>(object contextLocker, Action<T> changeContextAction)
+            where T : DbContext, new()
+        {
+            lock (contextLocker)
+            {
+                using (var dbContext = new T())
+                {
+                    changeContextAction(dbContext);
+                    return dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private static void LogEndOfRetrying(Exception exception)
+        {
+            ProcessSaveException(exception);
+            Logger.Warn("Saving of changes is failed.");
+        }
+
+        private static void ProcessSaveException(Exception exception)
+        {
+            LogException(exception);
+            switch (exception)
+            {
+                case DbUpdateException updateException:
+                    ProcessDbUpdateException(updateException);
+                    break;
+            }
+        }
+
+        private static void LogException(Exception exception)
+        {
+            var excForLog = exception;
+            while (excForLog.InnerException != null)
+            {
+                excForLog = excForLog.InnerException;
+            }
+            Logger.Error(excForLog);
+        }
+
+        private static void ProcessDbUpdateException(DbUpdateException exception)
+        {
+            var notSavedEntriesInfo = exception.Entries.Select(GetEntryInfo);
+            var message = string.Join("\n", notSavedEntriesInfo);
+            Logger.Warn(message);
+        }
+
+        private static string GetEntryInfo(DbEntityEntry entry)
+        {
+            const string valuesTemplate = "\n{0}:\n\t{1}";
+            var currentValuesInfo = GetPropertiesInfo(entry.CurrentValues);
+            var message = string.Format(valuesTemplate, "Current values", currentValuesInfo);
+            if (entry.State != EntityState.Modified)
+            {
+                return message;
+            }
+            var originalValuesInfo = GetPropertiesInfo(entry.OriginalValues);
+            message = message + string.Format(valuesTemplate, "Original values", originalValuesInfo);
+            return message;
+        }
+
+        private static string GetPropertiesInfo(DbPropertyValues properties)
+        {
+            var propertiesInfo = properties.PropertyNames.Select(name => $"{name} = {properties.GetValue<object>(name)}");
+            var message = string.Join(", ", propertiesInfo);
+            return message;
         }
     }
 }
