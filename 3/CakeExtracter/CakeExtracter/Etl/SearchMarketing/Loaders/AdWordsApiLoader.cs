daf8a4790d7114558e0f334d70e9e62ec50b78d6
Evtushenko Tatyana <t.evtushenko@itransition.com>
2019-01-10 17:50:20 +0300

    A little refactoring in AdwordsApiLoader.

--- 3/CakeExtracter/CakeExtracter/Etl/SearchMarketing/Loaders/AdWordsApiLoader.cs
+++ 3/CakeExtracter/CakeExtracter/Etl/SearchMarketing/Loaders/AdWordsApiLoader.cs
@@ -41,12 +41,12 @@ namespace CakeExtracter.Etl.SearchMarketing.Loaders
             else
                 return network.Substring(0, 1);
         }
+
         public static string Device_StringToLetter(string device)
         {
-            if (device == null)
-                return null;
             return string.IsNullOrEmpty(device) ? string.Empty : device.Substring(0, 1);
         }
+
         public static string ClickType_StringToLetter(string clickType)
         {
             var clickTypeAbbrev = clickType.Substring(0, 1);
