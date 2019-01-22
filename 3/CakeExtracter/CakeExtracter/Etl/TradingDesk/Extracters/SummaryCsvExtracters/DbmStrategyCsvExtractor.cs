762222996af5ef99d2414f36e1b254615d21d967
Evtushenko Tatyana <t.evtushenko@itransition.com>
2019-01-22 18:13:58 +0300

    Move the logic for parsing DBM strategy reports into a separate class.

---
+++ 3/CakeExtracter/CakeExtracter/Etl/TradingDesk/Extracters/SummaryCsvExtracters/DbmStrategyCsvExtractor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using DirectAgents.Domain.Entities.CPProg;
+
+namespace CakeExtracter.Etl.TradingDesk.Extracters.SummaryCsvExtracters
+{
+    internal class DbmStrategyCsvExtractor : TDStrategySummaryExtracter
+    {
+        private const string FirstColumnHeaderName = "Date";
+
+        public DbmStrategyCsvExtractor(ColumnMapping columnMapping, StreamReader streamReader = null, string csvFilePath = null) : base(columnMapping, streamReader, csvFilePath)
+        {
+        }
+
+        protected override bool ShouldReaderSkipRecord(string[] fields)
+        {
+            var isSkipRecord = base.ShouldReaderSkipRecord(fields);
+            var isFirstColumnIsNotDateTime = ShouldReaderSkipRecordNotDateTime(fields[0]);
+
+            return isSkipRecord || isFirstColumnIsNotDateTime;
+        }
+
+        /// <summary>
+        /// Method skips those records for which the value of the first column isn't Date Time (except for the title)
+        /// </summary>
+        /// <param name="firstColumn">The first column of a row from a CSV report, the value of the first column must be of a type DateTime</param>
+        /// <returns>True - skip the record; False - not skip the record</returns>
+        private bool ShouldReaderSkipRecordNotDateTime(string firstColumn)
+        {
+            if (string.Equals(firstColumn, FirstColumnHeaderName))
+            {
+                return false;
+            }
+            return !DateTime.TryParse(firstColumn, out _);
+        }
+    }
+}
