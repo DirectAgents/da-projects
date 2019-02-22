81c7195c7630e57a7f97f4042c4f7ff6c1d31a98
Oleg Harko <o.harko@outlook.com>
2019-02-22 14:39:02 +0300

    Added draft sql runner tool.

---
+++ 3/DirectAgents.Web/CakeExtractor.SqlScriptRunner/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CakeExtractor.SqlScriptsRunner
+{
+    class Program
+    {
+        private const string ConnectionStringConfigurationName = "DataBaseConnectionString";
+
+        static void Main(string[] args)
+        {
+            var connectionString = GetDataBaseConnectionString();
+            Console.WriteLine(connectionString);
+        }
+
+
+        private bool runSqlScriptFile(string fileName, string connectionString)
+        {
+            try
+            {
+                string script = File.ReadAllText(fileName);
+                
+                // split script on GO command
+                System.Collections.Generic.IEnumerable<string> commandStrings = Regex.Split(script, @"^\s*GO\s*$",
+                                         RegexOptions.Multiline | RegexOptions.IgnoreCase);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    foreach (string commandString in commandStrings)
+                    {
+                        if (commandString.Trim() != "")
+                        {
+                            using (var command = new SqlCommand(commandString, connection))
+                            {
+                                try
+                                {
+                                    command.ExecuteNonQuery();
+                                }
+                                catch (SqlException ex)
+                                {
+                                    string spError = commandString.Length > 100 ? commandString.Substring(0, 100) + " ...\n..." : commandString;
+                                    Console.WriteLine(string.Format("Please check the SqlServer script.\nFile: {0} \nLine: {1} \nError: {2} \nSQL Command: \n{3}", pathStoreProceduresFile, ex.LineNumber, ex.Message, spError), "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                                    return false;
+                                }
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        private static string GetDataBaseConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings[ConnectionStringConfigurationName].ConnectionString;
+        }
+    }
+}
