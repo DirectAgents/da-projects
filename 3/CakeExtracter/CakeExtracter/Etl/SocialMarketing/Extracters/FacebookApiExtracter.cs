693ec78cd919e670216df4023c5f9f3a94850a5a
Oleg Harko <o.harko@outlook.com>
2019-04-25 13:55:58 +0300

    Return back obsolete facebook command.

---
+++ 3/CakeExtracter/CakeExtracter/Etl/SocialMarketing/Extracters/FacebookApiExtracter.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using CakeExtracter.Common;
+using DirectAgents.Domain.Entities.CPProg;
+using FacebookAPI;
+using FacebookAPI.Entities;
+
+namespace CakeExtracter.Etl.SocialMarketing.Extracters
+{
+    public abstract class FacebookApiExtracter<T> : Extracter<T>
+    {
+        protected readonly FacebookUtility _fbUtility;
+        protected readonly DateRange? dateRange;
+        protected readonly int accountId;   // in our db
+        protected readonly string fbAccountId; // fb account: aka "ad account"
+
+        public FacebookApiExtracter(FacebookUtility fbUtility, DateRange? dateRange, ExtAccount account, bool includeAllActions = false)
+        {
+            this._fbUtility = fbUtility;
+            this._fbUtility.IncludeAllActions = includeAllActions;
+            this.dateRange = dateRange;
+            this.accountId = account.Id;
+            this.fbAccountId = account.ExternalId;
+        }
+    }
+
+    public class FacebookDailySummaryExtracter : FacebookApiExtracter<FBSummary>
+    {
+        public FacebookDailySummaryExtracter(DateRange dateRange, ExtAccount account, FacebookUtility fbUtility, bool includeAllActions = false)
+            : base(fbUtility, dateRange, account, includeAllActions)
+        { }
+
+        protected override void Extract()
+        {
+            Logger.Info(accountId, "Extracting DailySummaries from Facebook API for ({0}) from {1:d} to {2:d}",
+                        this.fbAccountId, this.dateRange.Value.FromDate, this.dateRange.Value.ToDate);
+            try
+            {
+                var fbSums = _fbUtility.GetDailyStats("act_" + fbAccountId, dateRange.Value.FromDate, dateRange.Value.ToDate);
+                Add(fbSums);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(accountId, ex);
+            }
+            End();
+        }
+    }
+
+    public class FacebookCampaignSummaryExtracter : FacebookApiExtracter<FBSummary>
+    {
+        public FacebookCampaignSummaryExtracter(DateRange dateRange, ExtAccount account, FacebookUtility fbUtility, bool includeAllActions = false)
+            : base(fbUtility, dateRange, account, includeAllActions)
+        { }
+
+        protected override void Extract()
+        {
+            Logger.Info(accountId, "Extracting Campaign Summaries from Facebook API for ({0}) from {1:d} to {2:d}",
+                        this.fbAccountId, this.dateRange.Value.FromDate, this.dateRange.Value.ToDate);
+            try
+            {
+                var fbSums = _fbUtility.GetDailyCampaignStats("act_" + fbAccountId, dateRange.Value.FromDate, dateRange.Value.ToDate);
+                Add(fbSums);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(accountId, ex);
+            }
+            End();
+        }
+    }
+
+    public class FacebookAdSetSummaryExtracter : FacebookApiExtracter<FBSummary>
+    {
+        public FacebookAdSetSummaryExtracter(DateRange dateRange, ExtAccount account, FacebookUtility fbUtility, bool includeAllActions = false)
+            : base(fbUtility, dateRange, account, includeAllActions)
+        { }
+
+        protected override void Extract()
+        {
+            Logger.Info(accountId, "Extracting AdSet Summaries from Facebook API for ({0}) from {1:d} to {2:d}",
+                        this.fbAccountId, this.dateRange.Value.FromDate, this.dateRange.Value.ToDate);
+            try
+            {
+                var fbSums = _fbUtility.GetDailyAdSetStats("act_" + fbAccountId, dateRange.Value.FromDate, dateRange.Value.ToDate);
+                Add(fbSums);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(accountId, ex);
+            }
+            End();
+        }
+    }
+
+    public class FacebookAdSummaryExtracter : FacebookApiExtracter<FBSummary>
+    {
+        public FacebookAdSummaryExtracter(DateRange dateRange, ExtAccount account, FacebookUtility fbUtility, bool includeAllActions = false)
+            : base(fbUtility, dateRange, account, includeAllActions)
+        { }
+
+        protected override void Extract()
+        {
+            Logger.Info(accountId, "Extracting Ad Summaries from Facebook API for ({0}) from {1:d} to {2:d}",
+                        this.fbAccountId, this.dateRange.Value.FromDate, this.dateRange.Value.ToDate);
+            try
+            {
+                var fbSums = _fbUtility.GetDailyAdStats("act_" + fbAccountId, dateRange.Value.FromDate, dateRange.Value.ToDate);
+                Add(fbSums);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(accountId, ex);
+            }
+            End();
+        }
+    }
+
+    public class FacebookAdPreviewExtracter : FacebookApiExtracter<FBAdPreview>
+    {
+        protected IEnumerable<string> fbAdIds;
+
+        public FacebookAdPreviewExtracter(ExtAccount account, IEnumerable<string> fbAdIds, FacebookUtility fbUtility)
+            : base(fbUtility, null, account)
+        {
+            this.fbAdIds = fbAdIds;
+        }
+
+        protected override void Extract()
+        {
+            Logger.Info(accountId, "Extracting Ad Previews from Facebook API for ({0})", this.fbAccountId);
+            //var fbAds = _fbUtility.GetAdPreviews("act_" + fbAccountId, fbAdIds);
+            try
+            {
+                var fbAds = _fbUtility.GetAdPreviews(fbAdIds);
+                Add(fbAds);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(accountId, ex);
+            }
+            End();
+        }
+    }
+}
---
+++ 3/CakeExtracter/CakeExtracter/Etl/SocialMarketing/LoadersDA/FacebookDailySummaryLoader.cs
@@ -0,0 +1,98 @@
+﻿using CakeExtracter.Helpers;
+using DirectAgents.Domain.Contexts;
+using DirectAgents.Domain.Entities.CPProg;
+using FacebookAPI;
+using FacebookAPI.Entities;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CakeExtracter.Etl.SocialMarketing.LoadersDA
+{
+    public class FacebookDailySummaryLoader : Loader<FBSummary>
+    {
+        public FacebookDailySummaryLoader(int accountId)
+            : base(accountId)
+        {
+            BatchSize = FacebookUtility.RowsReturnedAtATime; //FB API only returns 25 rows at a time
+        }
+
+        protected override int Load(List<FBSummary> items)
+        {
+            Logger.Info(accountId, "Loading {0} Facebook DailySummaries..", items.Count);
+            var count = UpsertDailySummaries(items);
+            return count;
+        }
+
+        private int UpsertDailySummaries(IEnumerable<FBSummary> items)
+        {
+            var progress = new LoadingProgress();
+            using (var db = new ClientPortalProgContext())
+            {
+                foreach (var item in items)
+                {
+                    var source = new DailySummary
+                    {
+                        AccountId = accountId,
+                        Date = item.Date,
+                        Impressions = item.Impressions,
+                        Clicks = item.LinkClicks,
+                        AllClicks = item.AllClicks,
+                        PostClickConv = item.Conversions_click,
+                        PostViewConv = item.Conversions_view,
+                        PostClickRev = item.ConVal_click,
+                        PostViewRev = item.ConVal_view,
+                        Cost = item.Spend
+                    };
+                    var target = db.Set<DailySummary>().Find(item.Date, accountId);
+                    if (target == null)
+                    {
+                        if (!item.AllZeros())
+                        {
+                            db.DailySummaries.Add(source);
+                            progress.AddedCount++;
+                        }
+                        else
+                        {
+                            progress.AlreadyDeletedCount++;
+                        }
+                    }
+                    else // DailySummary already exists
+                    {
+                        var entry = db.Entry(target);
+                        if (entry.State == EntityState.Unchanged)
+                        {
+                            if (!item.AllZeros())
+                            {
+                                entry.State = EntityState.Detached;
+                                AutoMapper.Mapper.Map(source, target);
+                                entry.State = EntityState.Modified;
+                                progress.UpdatedCount++;
+                            }
+                            else
+                            {
+                                entry.State = EntityState.Deleted;
+                            }
+                        }
+                        else
+                        {
+                            Logger.Warn(accountId, "Encountered duplicate DailySummary for {0:d}", item.Date);
+                            progress.DuplicateCount++;
+                        }
+                    }
+
+                    progress.ItemCount++;
+                }
+
+                SafeContextWrapper.TrySaveChanges(db);
+            }
+
+            Logger.Info(accountId, "Saving {0} DailySummaries ({1} updates, {2} additions, {3} duplicates, {4} deleted, {5} already-deleted)",
+                progress.ItemCount, progress.UpdatedCount, progress.AddedCount, progress.DuplicateCount, progress.DeletedCount, progress.AlreadyDeletedCount);
+            if (progress.DuplicateCount > 0)
+            {
+                Logger.Warn(accountId, "Encountered {0} duplicates which were skipped", progress.DuplicateCount);
+            }
+            return progress.ItemCount;
+        }
+    }
+}
