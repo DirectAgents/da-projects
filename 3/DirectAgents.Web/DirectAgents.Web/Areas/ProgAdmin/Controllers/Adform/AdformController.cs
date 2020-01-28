8ca384b29a27f0501020ee03fbd7e365f3dcc461
m.kozel <m.kozel@itransition.com>
2020-01-28 12:14:55 +0300

    DIRAGBA-340 Admin portal: created a new page Adform

---
+++ 3/DirectAgents.Web/DirectAgents.Web/Areas/ProgAdmin/Controllers/Adform/AdformController.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using DirectAgents.Domain.SpecialPlatformsDataProviders.Adform;
+using DirectAgents.Web.Areas.ProgAdmin.Models.Adform;
+
+namespace DirectAgents.Web.Areas.ProgAdmin.Controllers.Adform
+{
+    /// <summary>
+    /// Adform Stats Controller.
+    /// </summary>
+    /// <seealso cref="System.Web.Mvc.Controller" />
+    public class AdformController : Controller
+    {
+        private readonly IAdformWebPortalDataService dataService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdformController"/> class.
+        /// </summary>
+        /// <param name="dataService">The Adform web portal data service.</param>
+        public AdformController(IAdformWebPortalDataService dataService)
+        {
+            this.dataService = dataService;
+        }
+
+        /// <summary>
+        /// Endpoint for latests page.
+        /// </summary>
+        /// <param name="acctId">The acct identifier.</param>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <returns>Latests Action Result</returns>
+        public ActionResult Latests()
+        {
+            var latests = dataService.GetAccountsLatestsInfo();
+            var model = new AdformLatestsInfoVm
+            {
+                LatestsInfo = latests.ToList(),
+            };
+            return View(model);
+        }
+    }
+}
