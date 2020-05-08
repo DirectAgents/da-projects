519d8d953ea8f41a96030f05ab37015567081584
m.kozel <m.kozel@itransition.com>
2020-05-08 17:32:06 +0300

    DIRAGBA-405 temp commit

---
+++ 3/DirectAgents.Web/DirectAgents.Web/Areas/MatchPortal/Controllers/ProductMatchingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DirectAgents.Web.Areas.MatchPortal.Models;
+
+namespace DirectAgents.Web.Areas.MatchPortal.Controllers
+{
+    public class ProductMatchingController : Controller
+    {
+        // GET: MatchPortal/ProductMatching
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 3/DirectAgents.Web/DirectAgents.Web/Areas/MatchPortal/MatchPortalAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DirectAgents.Web.Areas.MatchPortal
+{
+    public class MatchPortalAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "MatchPortal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "MatchPortal_default",
+                "MatchPortal/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
