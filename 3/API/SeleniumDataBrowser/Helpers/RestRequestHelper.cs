19a810d337357d25f3e4e6e8edcc026a46186db1
m.kozel <m.kozel@itransition.com>
2020-09-01 18:57:04 +0300

    DIRAGBA-498 refactoring job

--- 3/API/SeleniumDataBrowser/Helpers/RestRequestHelper.cs
+++ 3/API/SeleniumDataBrowser/Helpers/RestRequestHelper.cs
@@ -15,16 +15,26 @@ namespace SeleniumDataBrowser.Helpers
             return response;
         }
 
+        public static IRestResponse<T> SendGetRequest<T>(string baseUrl, RestRequest request)
+            where T : new()
+        {
+            var restClient = new RestClient(baseUrl);
+            var response = restClient.ExecuteAsGet<T>(request, "GET");
+            return response;
+        }
+
         public static RestRequest CreateRestRequest(
             string relativePath,
             IEnumerable<Cookie> cookies,
             Dictionary<string, string> queryParams = null,
-            object body = null)
+            object body = null,
+            string token = null)
         {
             var request = CreateSimpleRequestObject(relativePath);
             SetCookies(request, cookies);
             SetQueryParameters(request, queryParams);
             SetBodyParameters(request, body);
+            SetToken(request, token);
             return request;
         }
 
@@ -52,6 +62,14 @@ namespace SeleniumDataBrowser.Helpers
             if (body != null)
             {
                 request.AddJsonBody(body);
+            }
+        }
+
+        private static void SetToken(IRestRequest request, string token)
+        {
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                request.AddHeader("X-CSRF-token", token);
             }
         }
     }
